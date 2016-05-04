d0627e4b938dadbbda4d94144b2b725bc5446cec
Wataru <xbee@dream.jp>
2016-05-05 05:57:57 +0900

    コメント追加

--- tools/udp_logger.sh
+++ tools/udp_logger.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#
 # UDPを受信する
-#
-echo "Listening UDP port "${1}"..."
-while [ ${1} -ge 1 ] && [ ${1} -lt 65536 ]
-do
-	UDP=`sudo netcat -luw0 ${1}`
-	DATE=`date "+%Y/%m/%d %R"`
-	echo $DATE, $UDP
-done
+# Copyright (c) 2016 Wataru KUNINO
+echo "UDP Logger (Usage: ${0} port)"            # タイトル表示
+echo "Listening UDP port "${1}"..."             # ポート番号表示
+while [ ${1} -ge 1 ] && [ ${1} -lt 65536 ]      # ポート番号の範囲確認
+do                                              # 繰り返し
+    UDP=`sudo netcat -luw0 ${1}`                # UDPパケットを取得
+    DATE=`date "+%Y/%m/%d %R"`                  # 日時を取得
+    echo $DATE, $UDP                            # 取得日時とデータを表示
+done                                            # 繰り返しここまで
