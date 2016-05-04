d96b6220645aa45ac3acd814ededa72117df957c
Wataru <xbee@dream.jp>
2016-05-05 06:26:33 +0900

    パラメータの入力値の確認を追加

--- tools/udp_logger.sh
+++ tools/udp_logger.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 # UDPを受信する
 # Copyright (c) 2016 Wataru KUNINO
-echo "UDP Logger (Usage: ${0} port)"            # タイトル表示
-echo "Listening UDP port "${1}"..."             # ポート番号表示
-while [ ${1} -ge 1 ] && [ ${1} -lt 65536 ]      # ポート番号の範囲確認
+
+echo "UDP Logger (usage: ${0} port)"            # タイトル表示
+if [ ${#} = 1 ]                                 # 入力パラメータ数の確認
+then                                            # 1つだった時
+    if [ ${1} -ge 1 ] && [ ${1} -le 65535 ]     # ポート番号の範囲確認
+    then                                        # 1以上65535以下の時
+        PORT=${1}                               # ポート番号を設定
+    else                                        # 範囲外だった時
+        PORT=1024                               # UDPポート番号を1024に
+    fi                                          # ifの終了
+else                                            # 1つでは無かった時
+    PORT=1024                                   # UDPポート番号を1024に
+fi                                              # ifの終了
+echo "Listening UDP port "${PORT}"..."          # ポート番号表示
+while true                                      # 永遠に
 do                                              # 繰り返し
-    UDP=`sudo netcat -luw0 ${1}`                # UDPパケットを取得
+    if [ ${PORT} -lt 1024 ]                     # ポート番号を確認
+    then                                        # 1024未満の時
+        UDP=`sudo netcat -luw0 ${PORT}`         # UDPパケットを取得
+    else                                        # ポート番号が1024以上の時
+        UDP=`netcat -luw0 ${PORT}`              # UDPパケットを取得
+    fi                                          # ifの終了
     DATE=`date "+%Y/%m/%d %R"`                  # 日時を取得
     echo $DATE, $UDP                            # 取得日時とデータを表示
 done                                            # 繰り返しここまで
