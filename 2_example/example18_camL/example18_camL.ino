927def6d2636bb67be53cf486893b7ff2e4f3eac
Wataru KUNINO <xbee@dream.jp>
2016-08-07 00:40:09 +0900

    改良

--- 2_example/example18_camL/example18_camL.ino
+++ 2_example/example18_camL/example18_camL.ino
@@ -11,25 +11,30 @@ Example 18: 監視カメラ for SparkFun SEN-11610 (LynkSprite JPEG Color Camera
 extern "C" {
 #include "user_interface.h"                 // ESP8266用の拡張IFライブラリ
 }
+#define PIN_CAM 13                          // IO 13(5番ピン)にPch-FETを接続する
 #define TIMEOUT 20000                       // タイムアウト 20秒
 #define SSID "1234ABCD"                     // 無線LANアクセスポイントのSSID
 #define PASS "password"                     // パスワード
-#define FILENAME "/cam.jpg"
+#define FILENAME "/cam.jpg"                 // 画像ファイル名(ダウンロード用)
 
-SoftwareSerial softwareSerial(12,13);       // IO12をRX,IO13をTXに設定
+SoftwareSerial softwareSerial(12,14);       // IO12(4)をRX,IO14(3)をTXに設定
 WiFiServer server(80);                      // Wi-Fiサーバ(ポート80=HTTP)定義
 int size=0;                                 // 画像データの大きさ(バイト)
 int update=60;                              // ブラウザのページ更新間隔(秒)
 
 void setup(){ 
+    lcdSetup(8,2);                          // 液晶の初期化(8桁×2行)
+    pinMode(PIN_CAM,OUTPUT);                // FETを接続したポートを出力に
+    digitalWrite(PIN_CAM,0);                // FETをLOW(ON)にする
     Serial.begin(9600);                     // 動作確認のためのシリアル出力開始
     Serial.println("Example 18 cam");       // 「Example 18」をシリアル出力表示
-    softwareSerial.begin(38400);
     wifi_set_sleep_type(LIGHT_SLEEP_T);     // 省電力モードに設定する
     WiFi.mode(WIFI_STA);                    // 無線LANをSTAモードに設定
     WiFi.begin(SSID,PASS);                  // 無線LANアクセスポイントへ接続
-    CamSendResetCmd();
-    delay(4000);
+    delay(100);                             // カメラの起動待ち
+    softwareSerial.begin(38400);            // カメラとのシリアル通信を開始する
+    CamSendResetCmd();                      // カメラへリセット命令を送信する
+    delay(4000);                            // 完了待ち(開始直後の撮影防止対策)
     while(WiFi.status() != WL_CONNECTED){   // 接続に成功するまで待つ
         delay(500);                         // 待ち時間処理
     }
@@ -43,10 +48,9 @@ void loop() {
     char c;                                 // 文字変数を定義
     char s[65];                             // 文字列変数を定義 65バイト64文字
     byte data[32];                          // 画像転送用の一時保存変数
-    boolean EndFlag=0;
     int len;                                // 文字列等の長さカウント用の変数
     int t=0;                                // 待ち受け時間のカウント用の変数
-    int i;
+    int i,j;
     
     client = server.available();            // 接続されたクライアントを生成
     if(client==0)return;                    // loop()の先頭に戻る
@@ -77,28 +81,23 @@ void loop() {
         return;                             // 処理の終了・loop()の先頭へ
     }
     if(strncmp(s,"GET /cam.jpg",12)==0){    // 画像取得指示の場合
-        CamSendTakePhotoCmd();              // カメラを撮影モードに設定
+        CamSendTakePhotoCmd();              // カメラで写真を撮影する
         client.println("HTTP/1.0 200 OK");                  // HTTP OKを応答
         client.println("Content-Type: image/jpeg");         // JPEGコンテンツ
         client.println("Connection: close");                // 応答後に閉じる
         client.println();                                   // ヘッダの終了
-        while(softwareSerial.available()>0) softwareSerial.read();
-        size=0;                             // 変数sizeを0バイトへ
-        while(!EndFlag){                    // 終了フラグが0の時に繰り返す
+        size=CamReadFileSize();             // ファイルサイズを読み取る
+        j=0;                                // 変数j(総受信長)を0に設定
+        while(j<size){                      // 終了フラグが0の時に繰り返す
             len = CamRead(data);            // カメラからデータを取得
-            if(len<0){
-                EndFlag=1; len *= -1;       // 取得値が負の時に終了
-            }
-            size += len;                    // データサイズの合算
+            j += len;                       // データサイズの合算
             for(i=0;i<len;i++) client.write((byte)data[i]); // データ送信
         }
-        CamStopTakePhotoCmd();              // 撮影の停止
         CamReadADR0();                      // 読み出しアドレスのリセット
-        client.println();                   // 改行を出力
         client.stop();                      // クライアントの切断
-        Serial.print(size);                 // ファイルサイズをシリアル出力表示
-        Serial.println("Bytes");            // シリアル出力表示
-        lcdPrintVal("Bytes",size);          // ファイルサイズを液晶へ表示
+        Serial.print(j);                    // ファイルサイズをシリアル出力表示
+        Serial.println(" Bytes");           // シリアル出力表示
+        lcdPrintVal("TX Bytes",size);       // ファイルサイズを液晶へ表示
         return;                             // 処理の終了・loop()の先頭へ
     }
     if(strncmp(s,"GET /?INT=",10)==0){      // 更新時間の設定命令を受けた時
@@ -107,31 +106,27 @@ void loop() {
     if(strncmp(s,"GET /?BPS=",10)==0){      // ビットレート設定命令時
         i = atoi(&s[10]);                   // 受信値を変数iに代入
         CamBaudRateCmd(i);                  // ビットレート設定
-        delay(100);
+        delay(100);                         // 処理完了待ち
         softwareSerial.begin(i);            // ビットレート変更
     }
     if(strncmp(s,"GET /?SIZE=",11)==0){     // JPEGサイズ設定命令時
         i = atoi(&s[11]);                   // 受信値を変数iに代入
         CamSizeCmd(i);                      // JPEGサイズ設定
-        delay(100);
+        delay(100);                         // 処理完了待ち
         CamSendResetCmd();                  // リセットコマンド
-        softwareSerial.begin(38400);
+        softwareSerial.begin(38400);        // シリアル速度を初期値に戻す
     }
     if(strncmp(s,"GET /?RATIO=",12)==0){    // 圧縮率設定命令時
         i = atoi(&s[12]);                   // 受信値を変数iに代入
         CamRatioCmd(i);                     // 圧縮率設定コマンド
     }
-    if(strncmp(s,"GET /?POWER=",12)==0){    // パワー設定命令時
-        i = atoi(&s[12]);                   // 受信値を変数iに代入
-        CamPowerCmd(i);                     // パワー設定コマンド
-    }
     if(strncmp(s,"GET /?RESET=",12)==0){    // リセット命令時
         CamSendResetCmd();                  // リセットコマンド
-        softwareSerial.begin(38400);
-        s[11]='\0';
+        softwareSerial.begin(38400);        // シリアル速度を初期値に戻す
+        s[11]='\0';                         // RESET=以降に続く文字を捨てる
     }
     for(i=6;i<strlen(s);i++) if(s[i]==' '||s[i]=='+') s[i]='\0';
-    htmlMesg(client,&s[6],WiFi.localIP()); 	// メッセージ表示
+    htmlMesg(client,&s[6],WiFi.localIP());  // メッセージ表示
     client.stop();                          // クライアント切断
     Serial.println("Sent HTML");            // シリアル出力表示
 }
--- 2_example/example18_camL/linkSprite_cam_IR.ino
+++ 2_example/example18_camL/linkSprite_cam_IR.ino
@@ -1,6 +1,6 @@
 /*******************************************************************************
-Linksprite社の LinkSprite_cam_IR.ino を基に国野 亘が改変し、下記のライセンスの基に
-配布します。
+Linksprite社の LinkSprite_cam_IR.ino を基に国野 亘が改変しました。
+下記のライセンスの基に配布します。
 
 本ソースコードは、CC BY-SA（表示 - 継承） です。
 https://creativecommons.org/licenses/by-sa/4.0/
@@ -16,19 +16,49 @@ void CamReadADR0(){
     camReadADR=0x0000;
 }
 
+int CamReadFileSize(){
+    byte incomingbyte;
+    int k,size=-1;
+    boolean EndFlag=0;
+    unsigned long t = millis();
+    byte ret[7]={0x76,0x00,0x34,0x00,0x04,0x00,0x00};
+    
+    while(softwareSerial.available()>0) softwareSerial.read();  // 読み捨てる
+    softwareSerial.write((byte)0x56);
+    softwareSerial.write((byte)0x00);
+    softwareSerial.write((byte)0x34);
+    softwareSerial.write((byte)0x01);
+    softwareSerial.write((byte)0x00);
+    delay(25);
+    k=0;
+    while(softwareSerial.available()>0){
+        incomingbyte=softwareSerial.read();
+        if(k<7)if(incomingbyte != ret[k])break;
+        if(k==7) size = (int)incomingbyte * 256;
+        if(k==8) size += (int)incomingbyte;
+        if(millis()-t>1000){
+            size=-1;    // 異常終了
+            break;
+        }
+        k++;
+        if(k>8) break;
+    }
+    while(softwareSerial.available()>0) softwareSerial.read();
+    return size;
+}
+
 int CamRead(byte *data){
     // 応答値は読み取りバイト数
-    // 応答値が負の時はEndFlag
     byte incomingbyte;
     int j,k,count;
-    boolean EndFlag=0;
+    boolean EndFlag=0;      // このフラグを使用する場合はグローバル変数に変更すること
     
-    j=0;
+    j=0;                    // jもkもcountも同じようなもの
     k=0;
     count=0;
     CamSendReadDataCmd();
-
     delay(25);
+    
     while(softwareSerial.available()>0){
         incomingbyte=softwareSerial.read();
         k++;
@@ -48,27 +78,29 @@ int CamRead(byte *data){
     }
     Serial.println();
     */
-    if(EndFlag) count *=-1;
+//    if(EndFlag) count *=-1;     // 終了時はcount値を負に反転する
     return count;
 }
 
 //Send Reset command
-void CamSendResetCmd()
-{
+void CamSendResetCmd(){
     softwareSerial.write((byte)0x56);
     softwareSerial.write((byte)0x00);
     softwareSerial.write((byte)0x26);
     softwareSerial.write((byte)0x00);
+    delay(25);
+    while(softwareSerial.available()>0) softwareSerial.read();
 }
 
 //Send take picture command
-void CamSendTakePhotoCmd()
-{
+void CamSendTakePhotoCmd(){
     softwareSerial.write((byte)0x56);
     softwareSerial.write((byte)0x00);
     softwareSerial.write((byte)0x36);
     softwareSerial.write((byte)0x01);
-    softwareSerial.write((byte)0x00);  
+    softwareSerial.write((byte)0x00);
+    delay(25);
+    while(softwareSerial.available()>0) softwareSerial.read();
 }
 
 //Read data
@@ -96,65 +128,92 @@ void CamSendReadDataCmd(){
     camReadADR+=0x20; //address increases 32 set according to buffer size
 }
 
-void CamStopTakePhotoCmd()
-{
-      softwareSerial.write((byte)0x56);
-      softwareSerial.write((byte)0x00);
-      softwareSerial.write((byte)0x36);
-      softwareSerial.write((byte)0x01);
-      softwareSerial.write((byte)0x03);        
+void CamStopTakePhotoCmd(){
+    softwareSerial.write((byte)0x56);
+    softwareSerial.write((byte)0x00);
+    softwareSerial.write((byte)0x36);
+    softwareSerial.write((byte)0x01);
+    softwareSerial.write((byte)0x03);        
+    delay(25);
+    while(softwareSerial.available()>0) softwareSerial.read();
 }
 
-void CamPowerCmd(int in)
-{
-      softwareSerial.write((byte)0x56);
-      softwareSerial.write((byte)0x00);
-      softwareSerial.write((byte)0x3E);
-      softwareSerial.write((byte)0x03);
-      softwareSerial.write((byte)0x00);
-      softwareSerial.write((byte)0x01);
-    if(in==1){  // Power ON
-      softwareSerial.write((byte)0x00);
+void CamPowerCmd(int in){
+    softwareSerial.write((byte)0x56);
+    softwareSerial.write((byte)0x00);
+    softwareSerial.write((byte)0x3E);
+    softwareSerial.write((byte)0x03);
+    softwareSerial.write((byte)0x00);
+    softwareSerial.write((byte)0x01);
+    if(in){  // Power ON
+        softwareSerial.write((byte)0x00);
+    }else{   // Power OFF (Power Save)
+        softwareSerial.write((byte)0x01);
+    }
+    delay(25);
+    while(softwareSerial.available()>0) softwareSerial.read();
+}
+
+void CamRatioCmd(int in){
+    softwareSerial.write((byte)0x56);
+    softwareSerial.write((byte)0x00);
+    softwareSerial.write((byte)0x31);
+    softwareSerial.write((byte)0x05);
+    softwareSerial.write((byte)0x01);
+    softwareSerial.write((byte)0x01);
+    softwareSerial.write((byte)0x12);
+    softwareSerial.write((byte)0x04);
+    if(in>=0 && in<=255){  // Power ON
+        softwareSerial.write((byte)in);
     }else{  // Power OFF (Power Save)
-      softwareSerial.write((byte)0x01);
+        softwareSerial.write((byte)0x36);
     }
+    delay(25);
+    while(softwareSerial.available()>0) softwareSerial.read();
 }
 
-
-void CamBaudRateCmd(int in)
-{
-      softwareSerial.write((byte)0x56);
-      softwareSerial.write((byte)0x00);
-      softwareSerial.write((byte)0x24);
-      softwareSerial.write((byte)0x03);
-      softwareSerial.write((byte)0x01);
-    if(in==115200){
-      softwareSerial.write((byte)0x0D);
-      softwareSerial.write((byte)0xA6);
+void CamBaudRateCmd(int in){
+    softwareSerial.write((byte)0x56);
+    softwareSerial.write((byte)0x00);
+    softwareSerial.write((byte)0x24);
+    softwareSerial.write((byte)0x03);
+    softwareSerial.write((byte)0x01);
+    if(in==9600){
+        softwareSerial.write((byte)0xAE);
+        softwareSerial.write((byte)0xC8);
+    }else if(in==19200){
+        softwareSerial.write((byte)0x56);
+        softwareSerial.write((byte)0xE4);
+    }else if(in==57600){
+        softwareSerial.write((byte)0x1C);
+        softwareSerial.write((byte)0x4C);
+    }else if(in==115200){
+        softwareSerial.write((byte)0x0D);
+        softwareSerial.write((byte)0xA6);
     }else{  // 38400 bps
-      softwareSerial.write((byte)0x2A);
-      softwareSerial.write((byte)0xF2);
+        softwareSerial.write((byte)0x2A);
+        softwareSerial.write((byte)0xF2);
     }
-
+    delay(25);
+    while(softwareSerial.available()>0) softwareSerial.read();
 }
 
-void CamSizeCmd(int in)
-{
-      softwareSerial.write((byte)0x56);
-      softwareSerial.write((byte)0x00);
-      softwareSerial.write((byte)0x31);
-      softwareSerial.write((byte)0x05);
-      softwareSerial.write((byte)0x04);
-      softwareSerial.write((byte)0x01);
-      softwareSerial.write((byte)0x00);
-      softwareSerial.write((byte)0x19);
-    if(in==0){      // 640 x 480
-      softwareSerial.write((byte)0x00);
-    }else if(in==2){    // 160 x 120 bps
-      softwareSerial.write((byte)0x22);
-    }else{
-      softwareSerial.write((byte)0x11);
+void CamSizeCmd(int in){
+    softwareSerial.write((byte)0x56);
+    softwareSerial.write((byte)0x00);
+    softwareSerial.write((byte)0x31);
+    softwareSerial.write((byte)0x05);
+    softwareSerial.write((byte)0x04);
+    softwareSerial.write((byte)0x01);
+    softwareSerial.write((byte)0x00);
+    softwareSerial.write((byte)0x19);
+    if(in==0){          // 640 x 480
+        softwareSerial.write((byte)0x00);
+    }else if(in==2){    // 160 x 120
+        softwareSerial.write((byte)0x22);
+    }else{              // 320 x 240
+        softwareSerial.write((byte)0x11);
     }
+    delay(25);
+    while(softwareSerial.available()>0) softwareSerial.read();
 }
-
-
