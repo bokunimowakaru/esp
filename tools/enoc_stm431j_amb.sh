2eb5159cb50ca47e3dba587362ca25397b3235e0
Wataru KUNINO <xbee@dream.jp>
2016-09-24 17:33:32 +0900

    Ambient連携用サンプル追加

---
+++ tools/enoc_stm431j_amb.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# EnOcean STM431J用 データロガー
+#
+# このスクリプトを実行するとSTM431Jから得られた温度データをAmbientに送信します。
+# [保存先=Ambient(https://ambidata.io/)]
+
+AmbientChannelId=100                                    # AmbientチャネルID
+AmbientWriteKey="0123456789abcdef"                      # ライトキー(16進数)
+HOST="54.65.206.59"                                     # 送信先アドレス
+DEV="ocean_1"                                           # デバイス名を定義
+
+stty -F /dev/ttyUSB0 57600 -icanon                      # シリアル設定
+while true; do                                          # 永久ループの開始
+    data=`timeout 1 cat /dev/ttyUSB0|od -An -v -tx1 -w1` # データ取得
+    if [ -n "$data" ]; then                             # データがあった場合
+        # echo $data                                    # データ列の表示
+        head=`echo $data|cut -d' ' -f1-5`               # 先頭5バイトを抽出
+        if [ "$head" == "55 00 0a 02 0a" ]; then        # データがあった場合
+            DATE=`date "+%Y/%m/%d %R"`                  # 日時を取得
+            TEMP=$(( 0x`echo $data|cut -d' ' -f14`))    # 14バイト目(温度)を抽出
+            RSSI=$(( 0x`echo $data|cut -d' ' -f18`))    # 18バイト目(RSSI)を抽出
+            TEMP=$(( (255 - $TEMP) * 400 / 255))        # 温度に変換(10倍値)
+            DEC=$(( $TEMP / 10))                        # 整数部
+            FRAC=$(( $TEMP - $DEC * 10))                # 小数部
+            echo -E $DATE, $DEC.$FRAC, -$RSSI|tee -a log_${DEV}.csv
+            DATA=\"d1\"\:\"$DEC.$FRAC\"                 # データ生成(温度)
+            DATA=${DATA},\"d2\"\:\"-$RSSI\"             # データ生成(RSSI)
+            JSON="{\"writeKey\":\"${AmbientWriteKey}\",${DATA}}"
+            curl -s ${HOST}/api/v2/channels/${AmbientChannelId}/data\
+                 -X POST -H "Content-Type: application/json" -d ${JSON}
+        fi                                              # 日時,温度の表示と保存
+    fi
+done                                                    # 永久に繰り返す
+exit                                                    # 終了
