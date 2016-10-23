7e36f9f2f3350f26d4566d7a80e9869a53b5f296
Wataru KUNINO <xbee@dream.jp>
2016-10-23 19:26:32 +0900

    比較処理のバグ修正とカメラ対応

--- tools/automail_setup.sh
+++ tools/automail_setup.sh
@@ -8,10 +8,25 @@ if [ -e /etc/ssmtp/ssmtp.conf ] && [ -e ~/.muttrc ]; then
 else
 	./gmail_setup.sh
 fi
+echo "-------------------------------------------------------------------------"
+echo "USBカメラ用のソフトウェアfswebcamをインストールしますか？"
+echo "詳細：https://www.sanslogic.co.uk/fswebcam/"
+echo "インストールする場合は「yes」を入力してください。"
+echo -n "yes/no >"
+read yes
+if [ $yes = "yes" ]; then
+sudo apt-get install fswebcam
+fi
+
+echo "-------------------------------------------------------------------------"
 echo "自動メール送信を設定します。"
 echo "自動メールの送信先を入力してください"
 read MAILTO
+if [ $yes = "yes" ]; then
 cat automail_rclocal.txt|sed "s/MAILTO=\"\"/MAILTO=\"$MAILTO\"/g" > automail_rclocal.txt~
+else
+cat automail_rclocam.txt|sed "s/MAILTO=\"\"/MAILTO=\"$MAILTO\"/g" > automail_rclocal.txt~
+fi
 sudo mv -b automail_rclocal.txt~ /etc/rc.local
 sudo chmod a+x /etc/rc.local
 echo "設定を完了しました。"
