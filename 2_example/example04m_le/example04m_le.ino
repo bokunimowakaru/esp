78f9e2ae24041814bada358a86673900ee3e7ebc
Wataru KUNINO <xbee@dream.jp>
2016-11-09 22:12:49 +0900

    取得値が近いときは送信しないサンプルの追加

--- 2_example/example04m_le/example04m_le.ino
+++ 2_example/example04m_le/example04m_le.ino
@@ -1,5 +1,6 @@
 /*******************************************************************************
-Example 4: 乾電池駆動に向けた低消費電力動作のサンプル
+Example 4m: 乾電池駆動に向けた低消費電力動作のサンプル
+[取得値が近いときは送信しない]
                                             Copyright (c) 2016 Wataru KUNINO
 *******************************************************************************/
 
@@ -13,21 +14,29 @@ extern "C" {
 #define PASS "password"                     // パスワード
 #define SENDTO "192.168.0.255"              // 送信先のIPアドレス
 #define PORT 1024                           // 送信のポート番号
-#define SLEEP_P 50*1000000                  // スリープ時間 50秒(uint32_t)
+#define SLEEP_P 59*1000000                  // スリープ時間 59秒(uint32_t)
+#define SLEEP_N 30                          // 最長スリープ時間 SLEEP_P×SLEEP_N
 
-int adc;                                	// 整数型変数adcを定義
+int adc;                                    // 整数型変数adcを定義
+extern int WAKE_COUNT;
 
 void setup(){                               // 起動時に一度だけ実行する関数
     int waiting=0;                          // アクセスポイント接続待ち用
-	int mem;								// RTCメモリからの数値データ保存用
+    int mem;                                // RTCメモリからの数値データ保存用
     pinMode(PIN_EN,OUTPUT);                 // センサ用の電源を出力に
+    digitalWrite(PIN_EN,HIGH);              // センサ用の電源をONに
     Serial.begin(9600);                     // 動作確認のためのシリアル出力開始
     Serial.println("Example 04 LE");        // 「Example 04」をシリアル出力表示
     adc = system_adc_read();                // AD変換器から値を取得
-    mem = readRtcInt();						// RTCメモリからの読みとる
-    mem = adc * 100 / mem;					// メモリ値に対するAD変換値を算出
-    if( mem > 95 && mem < 105 ) sleep();	// ±5%の範囲の場合はスリープ
-	writeRtcInt(adc);						// AD変換値をRTCメモリへ保存
+    digitalWrite(PIN_EN,LOW);               // センサ用の電源をOFFに
+    Serial.print("ADC = ");
+    Serial.println(adc);                    // 取得値をシリアル出力表示
+    mem = readRtcInt();                     // RTCメモリからの読みとる
+    if(WAKE_COUNT%SLEEP_N){                 // SLEEP_Nが0以外の時に以下を実行
+        if( mem ) mem = adc * 100 / mem;    // メモリ値に対するAD変換値を算出
+        if( mem>95 && mem<105 ) sleep();    // メモリ値と近い場合にスリープ
+    }
+    writeRtcInt(adc);                       // AD変換値をRTCメモリへ保存
     WiFi.mode(WIFI_STA);                    // 無線LANをSTAモードに設定
     WiFi.begin(SSID,PASS);                  // 無線LANアクセスポイントへ接続
     while(WiFi.status() != WL_CONNECTED){   // 接続に成功するまで待つ
@@ -42,13 +51,8 @@ void setup(){                               // 起動時に一度だけ実行す
 
 void loop() {
     WiFiUDP udp;                            // UDP通信用のインスタンスを定義
-    
-    digitalWrite(PIN_EN,HIGH);              // センサ用の電源をONに
-    delay(5);                               // 起動待ち時間
-    digitalWrite(PIN_EN,LOW);               // センサ用の電源をOFFに
     udp.beginPacket(SENDTO, PORT);          // UDP送信先を設定
     udp.println(adc);                       // 変数adcの値を送信
-    Serial.println(adc);                    // シリアル出力表示
     udp.endPacket();                        // UDP送信の終了(実際に送信する)
     sleep();
 }
--- 2_example/example04m_le/rtc_mem.ino
+++ 2_example/example04m_le/rtc_mem.ino
@@ -1,5 +1,5 @@
 /*******************************************************************************
-RTC内のメモリの読み書きドライバ
+RTC内のメモリの読み書きドライバ(整数値)
 
 下記のウェブサイトの情報に基づいて作成しました。
 
@@ -7,116 +7,48 @@ http://jiwashin.blogspot.jp/2016/07/esp30.html
 https://lowreal.net/2016/01/10/1
 *******************************************************************************/
 
-#include <Arduino.h>
-extern "C" {
-#include <user_interface.h>
+#define kOffset         65
+int WAKE_COUNT;
+
+struct RtcStorage{
+    char iot[4];
+    int count;
+    int data;
 };
 
-#define kOffset_CRC         65
-#define kMaxDataArea  (512 - 4 - sizeof(int))
-
-// system_rtc_mem_write() 先のブロックアドレス。
-// 4 bytes で align されており、先頭256bytes はシステム予約領域
-// 64 から書けるはずだが、65 以降でないとうまくいかなかった。。
-#define kOffset_CRC         65
-
-//  crc
-//  thanks : https://github.com/vinmenn/Crc16/blob/master/examples/Crc16_Example/Crc16_Example.ino
-//  below comment is from original source
-//Check routine taken from
-//http://web.mit.edu/6.115/www/miscfiles/amulet/amulet-help/xmodem.htm
-
-int calcCRC(char *ptr,int count){
-//  char *ptr = rtcStorage.buffer;
-//  int   count = sizeof(rtcStorage.buffer);
-	Serial.print("calc crc counter = ");
-	Serial.println(count);
-
-	int crc;
-	char i;
-	crc = 0;
-	while(--count >= 0){
-		crc = crc ^ (int) * ptr++ << 8;
-		i = 8;
-		do{
-			if(crc & 0x8000) crc = crc << 1 ^ 0x1021;
-			else crc = crc << 1;
-		}while(--i);
-	}
-	return crc;
-}
-
-typedef struct{
-  int crc;
-  char buffer[kMaxDataArea];
-}RtcStorage;
-
-RtcStorage rtcStorage;
-//
-//  Storageを読み込む。もしCRCが不一致なら初期化
-//
-char *readRtcStorage(){
-	bool ok;
-	int crc;
-
-	Serial.print("storage size = ");
-	Serial.println(sizeof(rtcStorage));
-	// データ読みこみ
-	ok = system_rtc_mem_read(kOffset_CRC, &rtcStorage, kMaxDataArea);
-	if(!ok){
-		Serial.println("system_rtc_mem_read failed");
-		return 0;
-	}
-	crc = calcCRC( rtcStorage.buffer, kMaxDataArea );
-	Serial.print("crc = ");
-	Serial.print(crc, HEX);
-	Serial.print(", stored crc = ");
-	Serial.println(rtcStorage.crc, HEX);
-	if(crc != rtcStorage.crc || rtcStorage.buffer[kMaxDataArea-1]!='\0'){
-		Serial.println("crc mismatch");
-		memset(rtcStorage.buffer, 0, kMaxDataArea);
-		crc = calcCRC( rtcStorage.buffer, kMaxDataArea );
-		Serial.print("new crc = ");
-		Serial.println(crc, HEX);
-		rtcStorage.crc = crc;
-		ok = system_rtc_mem_write(kOffset_CRC, &rtcStorage, sizeof(rtcStorage));
-		if(!ok){
-			Serial.println("readStorageAndInitIfNeeded : write fail");
-		}
-	}
-	return rtcStorage.buffer;
-}
-
-bool writeRtcStorage(char *in) {
-	bool ok;
-	memset(rtcStorage.buffer, 0, kMaxDataArea);
-	strncpy(rtcStorage.buffer,in,kMaxDataArea-1);
-	rtcStorage.crc = calcCRC( rtcStorage.buffer, kMaxDataArea );
-	ok = system_rtc_mem_write(kOffset_CRC, &rtcStorage, sizeof(rtcStorage));
-	if(!ok){
-		Serial.println("Error : writeStorage : system_rtc_mem_write fail");
-	}
-	return ok;
+void printRtcInt(char *iot,int count, int data,int size){
+    Serial.print("Ident = "); Serial.print(iot);
+    Serial.print(", Count = "); Serial.print(count);
+    Serial.print(", Value = "); Serial.print(data);
+    Serial.print(", Size  = "); Serial.println(size);
 }
 
 int readRtcInt(){
-	return atoi(readRtcStorage());
+    struct RtcStorage mem_init={"IoT",0,0};
+    struct RtcStorage mem_read={"...",0,0};
+    bool ok;
+
+    ok = system_rtc_mem_read(kOffset, &mem_read, sizeof(mem_read));
+    Serial.println("Read from RTC Memory");
+    printRtcInt(mem_read.iot, mem_read.count, mem_read.data, sizeof(mem_read));
+    if(!ok || strncmp(mem_init.iot,mem_read.iot,3) ){
+        Serial.println("Initializing...");
+        printRtcInt(mem_init.iot, mem_init.count, mem_init.data, sizeof(mem_init));
+        system_rtc_mem_write(kOffset, &mem_init, sizeof(mem_init));
+        mem_read.count=0;
+        if (!ok) Serial.println("readRtcInt : write fail");
+    }
+    WAKE_COUNT=mem_read.count+1;
+    return mem_read.data;
 }
 
-bool writeRtcInt(int in){
-	char s[12];
-	itoa(in,s,10);
-	return writeRtcStorage(s);
-}
+bool writeRtcInt(int in) {
+    struct RtcStorage mem={"IoT",WAKE_COUNT,in};
+    bool ok;
 
-/*
-float readRtcVal(){
-	return strtof(readRtcStorage(),0);
+    Serial.println("Write to RTC Memory");
+    printRtcInt(mem.iot, mem.count, mem.data, sizeof(mem));
+    ok = system_rtc_mem_write(kOffset, &mem, sizeof(mem));
+    if(!ok) Serial.println("Error : writeRtcInt : system_rtc_mem_write fail");
+    return ok;
 }
-
-bool writeRtcVal(float in){
-	char s[9];
-	dtostrf(in,8,2,s);
-	return writeRtcStorage(s);
-}
-*/
