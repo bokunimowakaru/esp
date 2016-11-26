9b7c52eba77dfd6117c7d627d9fc63707cbfda53
Wataru KUNINO <xbee@dream.jp>
2016-11-26 23:41:58 +0900

    バグ修正・ファイル転送機能追加

--- 2_example/example22a_jam/example22a_jam.ino
+++ 2_example/example22a_jam/example22a_jam.ino
@@ -13,8 +13,8 @@ extern "C" {
 }
 #include <WiFiUdp.h>                            // UDP通信を行うライブラリ
 #define TIMEOUT 20000                           // タイムアウト 20秒
-char SSID[17]="1234ABCD";                       // 無線LANアクセスポイントのSSID
-char PASS[17]="password";                       // パスワード
+char SSID[33]="1234ABCD";                       // 無線LANアクセスポイントのSSID
+char PASS[33]="password";                       // パスワード
 #define PORT 1024                               // 受信ポート番号
 #define BUF_N 4096                              // バッファサイズ
 #define NTP_SERVER "ntp.nict.jp"                // NTPサーバのURL
@@ -24,53 +24,99 @@ char PASS[17]="password";                       // パスワード
 WiFiUDP udp;                                    // UDP通信用のインスタンスを定義
 WiFiServer server(80);                          // Wi-Fiサーバ(ポート80)定義
 IPAddress ip(192,168,0,1);                      // APモード時のIPアドレス
+int WiFiStat=0;                                 // ESPサーバ状態
 char tx[BUF_N+1]="\0";                          // 送信バッファ
 char mj[65]="\0";                               // MJコマンドの格納用
 File file;
-unsigned long TIME;                             // 1970年からmillis()＝0までの秒数
+unsigned long TIME=0;                           // 1970年からmillis()＝0までの秒数
                                                 // ※現在時刻は TIME+millis()/1000
 FtpServer ftpSrv;
 
-void setup(){                                   // 起動時に一度だけ実行する関数
+int connect(){
     int t=0;
+    ssidpass_read();
+    if(strcmp(SSID,"1234ABCD")==0){
+        strcpy(tx,"'ERROR SSID\n");
+        return -2;
+    }
+    wifi_set_sleep_type(LIGHT_SLEEP_T);         // 省電力モードに設定する
+    WiFi.mode(WIFI_STA);                        // 無線LANをSTAモードに設定
+    WiFi.begin(SSID,PASS); delay(1000);         // 無線LANアクセスポイントへ接続
+    Serial.print("'Connecting to ");            // 「Connecting」を出力する
+    Serial.println(SSID);                       // SSIDを出力する
+    while(WiFi.status() != WL_CONNECTED){       // 接続に成功するまで待つ
+        delay(500);                             // 待ち時間処理
+        Serial.print("'");                      // 無線APへの接続プログレス表示
+        t++;
+        if(t>60){
+            WiFi.disconnect();
+            Serial.println(); delay(100);
+            Serial.println("'ERROR AP"); delay(100);
+            return -1;                          // タイムアウト時 -1
+        }
+    }
+    ip=WiFi.localIP();
+    Serial.println();                           // 改行を出力
+    Serial.print("'");                          // コメント命令を送信
+    Serial.println(ip); delay(100);             // IPアドレスを出力する
+    WiFiStat=1;
+    t=0;
+    while(TIME==0){
+        delay(500);                             // 待ち時間処理
+        TIME=getNTP(NTP_SERVER,NTP_PORT);       // NTPを用いて時刻を取得
+        tx[0]='\'';
+        time2txt(&tx[1],TIME);                  // 日時をテキストに変換する
+        strcat(tx,"\n");
+        t++;
+        if(t>30){
+            Serial.println("'ERROR NTP"); delay(100);
+            return -2;                          // タイムアウト時 -1
+        }
+    }
+    TIME-=millis()/1000;                        // 起動後の経過時間を減算
+    Serial.println("'NTP OK"); delay(100);      // NTP OK表示
+    WiFiStat=2;
+    server.begin();                             // TCPサーバを起動する
+    Serial.println("'HTTP Serv."); delay(100);  // 表示
+    WiFiStat=3;
+    udp.begin(PORT);                            // UDP通信御開始
+    Serial.println("'UDP listen"); delay(100);  // 表示
+    WiFiStat=4;
+    ftpSrv.begin(FTP_USER,FTP_PASS);            // username, password for ftp.
+    Serial.println("'FTP Serv."); delay(100);   // 表示
+    WiFiStat=5;
+    return 0;
+}
+
+void disconnect(){
+    if(WiFiStat==5){
+        // ftpSrv.stop();
+        WiFiStat--;
+    }
+    if(WiFiStat==4){
+        udp.stop();
+        WiFiStat--;
+    }
+    if(WiFiStat==3){
+        server.close();
+        WiFiStat--;
+    }
+    if(WiFiStat==2){
+        WiFiStat--;
+    }
+    if(WiFiStat==1){
+        WiFi.disconnect();
+        WiFiStat--;
+    }
+}
+
+void setup(){                                   // 起動時に一度だけ実行する関数
     Serial.begin(115200);                       // IchigoJamとの通信ポート
     delay(5000);                                // IchigoJamの起動・通信処理待ち
     Serial.write(25); Serial.write(16);         // 停止コマンドとDLEコードの送信
     Serial.println("cls:?\"ESP Wi-Fi");         // シリアル出力表示
     while(!SPIFFS.begin())delay(1000);          // ファイルシステムの開始
-    wifi_set_sleep_type(LIGHT_SLEEP_T);         // 省電力モードに設定する
-    if(strcmp(SSID,"1234ABCD")){
-        WiFi.mode(WIFI_STA);                    // 無線LANをSTAモードに設定
-        WiFi.begin(SSID,PASS); delay(1000);     // 無線LANアクセスポイントへ接続
-        Serial.print("'Connecting to ");        // 「Connecting」を出力する
-        Serial.print(SSID);                     // SSIDを出力する
-        Serial.println("\"");                   // コマンドとして実行する
-        while(WiFi.status() != WL_CONNECTED){   // 接続に成功するまで待つ
-            delay(500);                         // 待ち時間処理
-            Serial.print("'");                  // 無線APへの接続プログレス表示
-        }
-        Serial.println();delay(1000);           // 改行を出力
-        while(TIME==0){
-            TIME=getNTP(NTP_SERVER,NTP_PORT);   // NTPを用いて時刻を取得
-            tx[0]='\'';
-            time2txt(&tx[1],TIME);              // 日時をテキストに変換する
-            strcat(tx,"\n");
-        }
-        ip=WiFi.localIP();
-        TIME-=millis()/1000;                    // 起動後の経過時間を減算
-    }else{
-        WiFi.mode(WIFI_AP); delay(1000);
-        Serial.println("'AP Mode");
-        WiFi.softAPConfig(ip,ip,IPAddress(255,255,255,0));
-        WiFi.softAP(SSID,PASS); delay(3000);
-        ip=WiFi.softAPIP();
-        TIME=0; delay(100);
-    }
-    server.begin();                             // TCPサーバを起動する
-    udp.begin(PORT);                            // UDP通信御開始
-    Serial.print("' ");                         // コメント命令を送信
-    Serial.println(ip);                         // IPアドレスを出力する
-    ftpSrv.begin(FTP_USER,FTP_PASS);            // username, password for ftp.
+    connect();                                  // SSIDが初期状態以外の時に接続
 }
 
 void loop(){                                    // 繰り返し実行する関数
@@ -81,7 +127,7 @@ void loop(){                                    // 繰り返し実行する関�
     char date[20];                              // 日付データ格納用
     char com[65]="\0";                          // IchigoJamへの送信コマンド
     char rx[65]="起動しました";                 // IchigoJamからの応答(表示用)
-    int i;
+    int i,j;
     int len=0;                                  // 文字列長を示す変数を定義
     int rxi=0;                                  // IchigoJamからの応答文字長
     int t=0;                                    // 待ち受け時間カウント用の変数
@@ -96,34 +142,78 @@ void loop(){                                    // 繰り返し実行する関�
     }
     time2txt(date,TIME+time/1000);              // 日時をテキストに変換する
     ftpSrv.handleFTP();                         // make sure in loop you call
-    s[1]='\0';
     if(Serial.available()){
-        s[0]=Serial.read();
-        if( mj[0]=='\0' ){
-            if(s[0] == '\'') strcat(mj,"'");
-        }else if( mj[0]!='\'' ) mj[0]='\0';
-        else{
-            if( s[0] == ' '){
-                if( mj[strlen(mj)-1] !=' ' ) strcat(mj," ");
-            }else if( s[0] == '\n'){
-                // MJコマンド
-                if( strncmp(mj,"'MJ APC ",7)){
-                    WiFi.disconnect(); delay(500);
-                    ssidpass_write(&mj[7]);
-                    WiFi.begin(SSID,PASS); delay(500);
+        c=Serial.read();
+        if(c=='\n'||c=='\r'){
+            // MJコマンド
+            if(strncmp(mj,"MJ APC ",7)==0){
+                disconnect();
+                ssidpass_write(&mj[7]);
+                connect();
+            }else if(strcmp(mj,"MJ FORMAT")==0){
+                SPIFFS.format();                // ファイル全消去
+                ssidpass_init();                // 設定ファイル保存
+                strcpy(tx,"'DONE FORMAT\n");    // シリアル出力表示
+            }else if(strcmp(mj,"MJ DATE")==0){
+                tx[0]='\'';
+                strcat(tx,date);
+                strcat(tx,"\n");
+            }else if(strcmp(mj,"MJ FILES")==0){
+                Dir dir = SPIFFS.openDir("/");
+                while(dir.next()){
+                    strcat(tx,"'MJ LOAD ");
+                    String string=dir.fileName();
+                    string.toCharArray(s,32);
+                    if(strlen(s)>1) strcat(tx,&s[1]);
+                    strcat(tx,"\n");
+                    if( strlen(tx) > BUF_N - 64 ) break;
                 }
-            }else if( s[0] != '\r') strcat(mj,s);
-            if( strlen(mj)>=64 ) mj[0]='\0';
+                if(!strlen(tx))strcpy(tx,"'no files\n");
+            }else if(strncmp(mj,"MJ LOAD ",8)==0){
+                mj[7]='/';
+                file = SPIFFS.open(&mj[7],"r");
+                if( strcmp(&mj[strlen(mj)-4],".txt")==0 ||
+                    strcmp(&mj[strlen(mj)-4],".TXT")==0 ||
+                    strcmp(&mj[strlen(mj)-4],".bas")==0 ||
+                    strcmp(&mj[strlen(mj)-4],".BAS")==0){
+                    tx[0]='\0'; i=0; j=0;
+                }else{
+                    tx[0]='\''; i=1; j=1;
+                }
+                if(file){
+                    while(file.available()){
+                        c=file.read();
+                        if(c=='\r') continue;
+                        tx[i]=c;
+                        i++;
+                        tx[i]='\0';
+                        if(i>=BUF_N) break;
+                        if(c=='\n' && j ){
+                            tx[i]='\'';
+                            i++;
+                            tx[i]='\0';
+                            if(i>=BUF_N) break;
+                        }
+                    }
+                    file.close();
+                    if(i>0)if(tx[i-1]=='\'')tx[i-1]='\0';
+                }else{
+                    strcpy(tx,"'no File\n");
+                }
+            }
+            mj[0]='\0';
+        }else if(c==' '){
+            if( mj[strlen(mj)-1] !=' ' ) strcat(mj," ");
+        }else if(isgraph(c)){
+            s[0]=c; s[1]='\0';
+            strcat(mj,s);
         }
+        if( strlen(mj)>=63 ) mj[63]='\0';
     }
     client = server.available();                // TCPクライアントを生成
     if(client==0){                              // TCPクライアントが無かった場合
         if(tx[0]){                              // 変数txに代入されていた場合
-            c=trUri2c(tx[0]);                   // URIエンコード空白文字の変換
-            if(c=='%'){                         // URIエンコード文字の検出
-                c=trUri2s(tx);                  // アスキー文字へ変換して変数Cへ
-            }
-            Serial.write(c); delay(18);         // IchigoJamへ出力
+            Serial.write(tx[0]); delay(18);     // IchigoJamへ出力
             if(c=='\n') delay(100);             // IchigoJamの処理待ち
             trShift(tx,1);                      // FIFOバッファのシフト処理
         }else{
@@ -133,17 +223,17 @@ void loop(){                                    // 繰り返し実行する関�
             memset(s, 0, 65);                   // 文字列変数sの初期化(65バイト)
             udp.read(s, 64);                    // UDP受信データを文字列変数sへ代入
             utf_del_uni(s);                     // UTF8の制御コードの除去
-            Serial.print("'");                  // コメント命令を送信
-            Serial.print(date);                 // 日時を出力する
-            Serial.print(",");                  // コメント命令を送信
-            strncpy(tx,s,BUF_N-1);
-            strcat(tx,"\n");
-            file = SPIFFS.open("log.txt","a");  // 追記保存のためにファイルを開く
+            file = SPIFFS.open("/log.csv","a"); // 追記保存のためにファイルを開く
             if(file==0)return;                  // ファイルを開けれなければ戻る
             for(i=0;i<64;i++) if(s[i]=='\r'||s[i]=='\n') s[i]='\0';
+            strcpy(tx,"'");
             file.print(date);                   // 日時を出力する
+            strcat(tx,date);
             file.print(',');                    // 「,」カンマをファイル出力
+            strcat(tx,",");
             file.println(s);                    // 受信データをファイル出力
+            strcat(tx,s);
+            strcat(tx,"\n");
             file.close();                       // ファイルを閉じる
         }
         return;                                 // loop()の先頭に戻る
@@ -225,7 +315,9 @@ void loop(){                                    // 繰り返し実行する関�
                     tx[i]=client.read();
                 }else break;
             }
+            trUri2txt(tx);
         }
+        html(client,"",rx,ip,date);
         Serial.write(16);                       // IchigoJamへDLEコードを送信
     }
     if(!client.connected())return;              // 切断された場合はloop()の先頭へ
@@ -257,7 +349,7 @@ void loop(){                                    // 繰り返し実行する関�
         }
         Serial.print("'HTTP Sends ");           // ファイルサイズの表示
         Serial.print(i);                        // ファイルサイズの表示
-        Serial.println("Bytes");                // シリアル出力表示
+        Serial.println(" Bytes");               // シリアル出力表示
     }
     client.stop();                              // クライアントの切断
 }
