80199188c2cfb5bcced73ca9f6592c253278d289
Wataru KUNINO <xbee@dream.jp>
2016-12-14 22:51:35 +0900

    Write enebleをYesにする

--- tools/ftp_setup.sh
+++ tools/ftp_setup.sh
@@ -8,4 +8,8 @@
 
 echo "vsftpdをインストールします"
 sudo apt-get install vsftpd
+sudo service vsftpd stop
+cat /etc/vsftpd.conf|sed -e "s/#write_enable=YES/write_enable=YES/g" > vsftpd.tmp
+sudo mv -b vsftpd.tmp /etc/vsftpd.conf
+sudo chown root:root /etc/vsftpd.conf
 sudo service vsftpd start
