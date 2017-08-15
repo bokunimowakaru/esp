c2b192fe2881c54c97470cd3660982ae28844943
Wataru KUNINO <xbee@dream.jp>
2017-08-15 21:26:17 +0900

    バグ修正

--- tools/aquestalk_tsig.sh
+++ tools/aquestalk_tsig.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # 現在の時刻を再生します。
 #
 # 本プログラムを実行するには、AQUEST社の AquesTalkPiが必要です。
@@ -8,16 +8,17 @@
 
 TIME_START=6                                # 時報の開始時刻(6:00～)
 TIME_END=22                                 # 時報の終了時刻(～21:59)
-HOUR=`date "+_H"`                           # 現在の時刻(時)
-MIN=`date "+%_M`                            # 現在の時刻(分)
+HOUR=`date "+%_H"`                          # 現在の時刻(時)
+MIN=`date "+%_M"`                           # 現在の時刻(分)
 if [ ${HOUR} -ge ${TIME_START} ] && [ ${HOUR} -le ${TIME_END} ]; then
     kill `pidof aplay` &> /dev/null         # 再生中の音声を終了
     sleep 0.5
     if [ ${MIN} -eq 0 ]; then
-        # 毎時0分のときは「分」を省略して再生する
-        echo ${HOUR}"時です。"|/home/pi/esp/tools/aquestalkpi/AquesTalkPi |aplay
+        # 毎時0分のときは「分」を省略して再生
+        TALK=${HOUR}"時です。"
     else
-        # 毎時0分以外の時は「時」と「分」を再生する
-        echo ${HOUR}"時"${MIN}"分です。"|/home/pi/esp/tools/aquestalkpi/AquesTalkPi |aplay
+        # 毎時0分以外の時は「時」と「分」を再生
+        TALK=${HOUR}"時"${MIN}"分です。"
     fi
+    /home/pi/esp/tools/aquestalkpi/AquesTalkPi ${TALK}|aplay
 fi
