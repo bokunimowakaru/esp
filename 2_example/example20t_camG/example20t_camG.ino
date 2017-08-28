3b3e1173698c3e840737e925a491a33b4832563d
Wataru KUNINO <xbee@dream.jp>
2017-08-28 21:03:21 +0900

    ESP8266WebServerライブラリを使用することにより動作を安定化

--- 2_example/example20t_camG/example20t_camG.ino
+++ 2_example/example20t_camG/example20t_camG.ino
@@ -1,6 +1,7 @@
 /*******************************************************************************
-Example 20: 監視カメラ for SeeedStudio Grove Serial Camera Kit
-画像一覧表示バージョン [試作品・不具合あり]
+Example 20t: 監視カメラ for SeeedStudio Grove Serial Camera Kit
+
+画像一覧表示バージョン [ESP8266WebServerライブラリ使用・完成版]
 
                                           Copyright (c) 2016-2017 Wataru KUNINO
 *******************************************************************************/
@@ -8,14 +9,13 @@ Example 20: 監視カメラ for SeeedStudio Grove Serial Camera Kit
 表示可能な写真の枚数について：
 Arduino IDEのタブ「html」をクリックするとコンテンツのスケッチを表示することが
 できます。スケッチ先頭の「#define PICT_NUM」を変更することで、写真の表示枚数を
-変更することが出来ます。初期値は5枚です。
-ブラウザからの応答が無くなったり、写真が表示されない場合は表示枚数を減らしてく
-ださい。リソース等に余裕を確保すれば、表示増やすことも可能かもしれません。
+変更することが出来ます。初期値は6枚です。
 */
 
 #include <SoftwareSerial.h>
 #include <FS.h>
 #include <ESP8266WiFi.h>                    // ESP8266用ライブラリ
+#include <ESP8266WebServer.h>
 #include <WiFiUdp.h>                        // UDP通信を行うライブラリ
 #define PIN_CAM 13                          // IO 13(5番ピン)にPch-FETを接続する
 #define TIMEOUT 20000                       // タイムアウト 20秒
@@ -24,13 +24,49 @@ Arduino IDEのタブ「html」をクリックするとコンテンツのスケ�
 
 File file;
 SoftwareSerial softwareSerial(12,14);       // IO12(4)をRX,IO14(3)をTXに設定
-WiFiServer server(80);                      // Wi-Fiサーバ(ポート80=HTTP)定義
-int size=0;                                 // 画像データの大きさ(バイト)
+ESP8266WebServer server(80);                // Wi-Fiサーバ(ポート80=HTTP)定義
+int photo_size=0;                           // 画像データの大きさ(バイト)
 int update=60;                              // ブラウザのページ更新間隔(秒)
 int photo_n=0;                              // 写真番号
 unsigned long shot_time=0;                  // 次回の撮影時間
 
-void setup(){ 
+void handleRoot() {
+    char html[2048];
+    Serial.print("HTTP");
+    
+    if(server.hasArg("SHOT")){
+        shot_time=1;
+        Serial.print(" SHOT");
+    }
+    if(server.hasArg("INT")){
+        String val=server.arg("INT");
+        update=val.toInt();
+        if(update < 10) update = 10;
+        Serial.print(" INT=");
+        Serial.print(update);
+    }
+    if(server.hasArg("SIZE")){
+        String val=server.arg("SIZE");
+        CamSizeCmd(val.toInt());
+        Serial.print(" SIZE=");
+        Serial.print(val.toInt());
+    }
+    if(server.hasArg("FORMAT")){
+        photo_n=0;
+        lcdPrint2("FORMAT");                // フォーマットの開始表示
+        SPIFFS.format();                    // ファイル全消去
+        Serial.print(" FORMAT");
+    }
+    getHtml(html,2047);
+    server.send ( 200, "text/html", html );
+    Serial.println(" Done");
+}
+
+void handleNotFound() {
+    sendError404();
+}
+
+void setup() { 
     lcdSetup(8,2);                          // 液晶の初期化(8桁×2行)
     pinMode(PIN_CAM,OUTPUT);                // FETを接続したポートを出力に
     digitalWrite(PIN_CAM,0);                // FETをLOW(ON)にする
@@ -50,119 +86,40 @@ void setup(){
     while(WiFi.status() != WL_CONNECTED){   // 接続に成功するまで待つ
         delay(500);                         // 待ち時間処理
     }
+    server.on ( "/", handleRoot );
+    server.serveStatic("/", SPIFFS, "/", "max-age=60000");
+    server.onNotFound ( handleNotFound );
     server.begin();                         // サーバを起動する
     Serial.println(WiFi.localIP());         // 本機のIPアドレスをシリアル表示
-    lcdPrintIp(WiFi.localIP());             // 本機のIPアドレスを液晶に表示
 }
 
 void loop() {
-    WiFiClient client;                      // Wi-Fiクライアントの定義
-    char c;                                 // 文字変数を定義
-    char s[65];                             // 文字列変数を定義 65バイト64文字
-    int len=0;                              // 文字列等の長さカウント用の変数
-    int t=0;                                // 待ち受け時間のカウント用の変数
-    int i;
     char filename[]="/cam***.jpg";          // 画像ファイル名(ダウンロード用)
-                                            // filename[4-6]は写真番号100～
+    int size;
 
-    client = server.available();            // 接続されたクライアントを生成
-    if(client==0){
-        if(shot_time > 0 && millis()/1000 < shot_time) return;
-        sprintf(filename,"/cam%03d.jpg",photo_n);
-        file = SPIFFS.open(filename,"w");   // 保存のためにファイルを開く
-        if(file){                           // ファイルを開けれなければ戻る
-            lcdPrint("Cam Capt");           // 「Cam Capt」を液晶に表示
-            lcdPrint2(filename);            // ファイル名を液晶に表示
-            Serial.print("Capturing ");     // シリアル出力表示
-            Serial.println(filename);
-            CamSizeCmd(2);                  // JPEGサイズを最小160x120に
-            delay(100);
-            CamCapture();                   // カメラで写真を撮影する
-            size=CamGetData();              // 写真をファイルへ保存する
-            file.close();                   // ファイルを閉じる
-            lcdPrint("Done Cap");           // 「Done Cap」を液晶に表示
-            Serial.print(size);             // ファイルサイズをシリアル出力表示
-            Serial.println(" Bytes");       // シリアル出力表示
-            photo_n++; if(photo_n>199) photo_n=0;
-        }else Serial.println("File open ERROR");
-        shot_time=millis()/1000ul+(unsigned long)update; // 次回の撮影時刻を設定
-        return;
+    server.handleClient();                  // HTTPサーバ処理
+    if(shot_time > 0){                      // 初回動作時以外のとき
+        if(millis()/1000<shot_time) return; // 撮影時刻では無いのでloopへ戻る
+        if(millis()/1000<update) return;    // 桁あふれ時の対策
     }
-    Serial.println("Connected");            // シリアル出力表示
-    while(client.connected()){              // 当該クライアントの接続状態を確認
-        if(client.available()){             // クライアントからのデータを確認
-            t=0;                            // 待ち時間変数をリセット
-            c=client.read();                // データを文字変数cに代入
-            if(c=='\n'){                    // 改行を検出した時
-                if(len>5 && strncmp(s,"GET /",5)==0) break;
-                len=0;                      // 文字列長を0に
-            }else if(c!='\r' && c!='\0'){
-                s[len]=c;                   // 文字列変数に文字cを追加
-                len++;                      // 変数lenに1を加算
-                s[len]='\0';                // 文字列を終端
-                if(len>=64) len=63;         // 文字列変数の上限
-            }
-        }
-        t++;                                // 変数tの値を1だけ増加させる
-        if(t>TIMEOUT) break; else delay(1); // TIMEOUTに到達したらwhileを抜ける
-    }
-    delay(10);
-    client.flush();                         // バッファ内のデータを破棄する
-    if(!client.connected()||len<6) return;  // 切断された場合はloop()の先頭へ
-    Serial.print(s);                        // 受信した命令をシリアル出力表示
-    lcdPrint(&s[5]);                        // 受信した命令を液晶に表示
-    if(strncmp(s,"GET / ",6)==0){           // コンテンツ取得命令時(撮影指示)
-        html(client,size,update,WiFi.localIP()); // コンテンツ表示
-        client.stop();                      // クライアントの切断
-        Serial.println(" Done");
-        return;                             // 処理の終了・loop()の先頭へ
-    }
-    if(strncmp(s,"GET /cam",8)==0 && strncmp(&s[11],".jpg",4)==0){  // 画像取得
-        strncpy(filename,&s[4],11);
-        lcdPrint(filename);                     // ファイル名を液晶へ出力表示
-        file = SPIFFS.open(filename,"r");       // 読み込みのためにファイルを開く
-        if(file){                               // ファイルを開けることが出来た時、
-            client.println("HTTP/1.0 200 OK");                  // HTTP OKを応答
-            client.print("Content-Length: ");                   // コンテンツ大きさ
-            client.println( file.size() );
-            client.println("Content-Type: image/jpeg");         // JPEGコンテンツ
-            client.println("Connection: close");                // 応答後に閉じる
-            client.println();                                   // ヘッダの終了
-            t=0; while(file.available()){       // ファイルがあれば繰り返し処理実行
-                s[t]=file.read(); t++;          // ファイルの読み込み
-                if(t >= 64){                    // 64バイトに達した時に転送処理
-                    if(!client.connected()) break;              // 接続状態確認
-                    client.write( (byte *)s, 64);               // 64バイト送信
-                    t=0; delay(1);                              // 送信完了待ち
-                }
-            }
-            if(t>0 && client.connected()) client.write((byte *)s,t);
-            file.close();                       // ファイルを閉じる
-        }else{
-            client.println("HTTP/1.0 404 Not Found");           // Not Foundを応答
-            client.println();                                   // ヘッダの終了
-        }
-        client.stop();                      // クライアントの切断
-        Serial.println(" Done");
-        return;                             // 処理の終了・loop()の先頭へ
-    }
-    if(strncmp(s,"GET /?SHOT",10)==0){      // 撮影コマンド
-        shot_time=0;
-    }
-    if(strncmp(s,"GET /?INT=",10)==0){      // 更新時間の設定命令を受けた時
-        update = atoi(&s[10]);              // 受信値を変数updateに代入
-    }
-    if(strncmp(s,"GET /?SIZE=",11)==0){     // JPEGサイズ設定命令時
-        i = atoi(&s[11]);                   // 受信値を変数iに代入
-        CamSizeCmd(i);                      // JPEGサイズ設定
-    }
-    if(strncmp(s,"GET /?FORMAT",12)==0){    // ファイルシステム初期化コマンド
-        lcdPrint2("FORMAT");                // フォーマットの開始表示
-        SPIFFS.format();                    // ファイル全消去
-        photo_n=0;
-    }
-    for(i=6;i<strlen(s);i++) if(s[i]==' '||s[i]=='+') s[i]='\0';
-    htmlMesg(client,&s[6],WiFi.localIP());  // メッセージ表示
-    Serial.println(" Done");
-    client.stop();                          // クライアント切断
+    sprintf(filename,"/cam%03d.jpg",photo_n);
+    file = SPIFFS.open(filename,"w");       // 保存のためにファイルを開く
+    if(file){                               // ファイルを開けれなければ戻る
+        lcdPrint("Cam Capt");               // 「Cam Capt」を液晶に表示
+        lcdPrint2(filename);                // ファイル名を液晶に表示
+        Serial.print("Capturing ");         // シリアル出力表示
+        Serial.println(&filename[1]);
+        CamSizeCmd(2);                      // JPEGサイズを最小160x120に
+        delay(100);
+        CamCapture();                       // カメラで写真を撮影する
+        size=CamGetData();                  // 写真をファイルへ保存する
+        file.close();                       // ファイルを閉じる
+        Serial.print(size);                 // ファイルサイズをシリアル出力表示
+        Serial.print(" Bytes ");            // シリアル出力表示
+        photo_n++; if(photo_n>199) photo_n=0;
+        photo_size=size;
+    }else Serial.println("File open ERROR");
+    shot_time=millis()/1000ul+(unsigned long)(update-5); // 次回の撮影時刻
+    Serial.println(", Done Capturing");
+    lcdPrintIp(WiFi.localIP());             // 本機のIPアドレスを液晶に表示
 }
--- 2_example/example20t_camG/html.ino
+++ 2_example/example20t_camG/html.ino
@@ -2,12 +2,16 @@
 防犯カメラ
                                            Copyright (c) 2016-2017 Wataru KUNINO
 *******************************************************************************/
-#define PICT_NUM 5 // 表示枚数(筆者の環境では最大5枚まででした)
+
+#define PICT_NUM 6     // 表示枚数 3～6枚程度(6枚以上でエラーが発生しやすくなる)
+extern int photo_size;
+extern int update;
 extern int photo_n;
 
-void html(WiFiClient &client, int size, int update, uint32_t ip){
-    char s[65],s_ip[16];
+void getHtml(char *html,int size) {
+    char s_ip[16];
     int i;
+    uint32_t ip = WiFi.localIP();
     
     sprintf(s_ip,"%i.%i.%i.%i",
         ip & 255,
@@ -15,92 +19,64 @@ void html(WiFiClient &client, int size, int update, uint32_t ip){
         ip>>16 & 255,
         ip>>24
     );
-    client.println("HTTP/1.0 200 OK");              // HTTP OKを応答
-    client.println("Content-Type: text/html");      // HTMLコンテンツ
-    client.println("Connection: close");            // 応答終了後にセッションを閉じる
-    client.println();
-    client.println("<html>");
-    client.println("<head><title>Test Page</title>");
-    client.println("<meta http-equiv=\"Content-type\" content=\"text/html; charset=UTF-8\">");
-    if(update){
-        client.print("<meta http-equiv=\"refresh\" content=\"");
-        client.print(update);
-        client.print(";URL=http://");
-        client.print(s_ip);
-        client.println("/\">");
-    }
-    client.println("</head>");
-    if(client.available())return;
-    if(!client.connected())return;
-    client.println("<body>");
-    client.println("<h3>防犯カメラ STATUS</h3>");
+    snprintf(html, size,"\
+    <html>\n<head>\n<title>Test Page</title>\n\
+    <meta http-equiv=\"Content-type\" content=\"text/html; charset=UTF-8\">\n\
+    <meta http-equiv=\"refresh\" content=\"%d\";URL=http://%s/\">\n</head>\n\
+    <body>\n<h3>防犯カメラ STATUS</h3>\
+    ",update,s_ip);
     for(i=(PICT_NUM-1);i>=0;i--){
         if(photo_n-i-1>=0){
-            sprintf(s,"<img width=160 height=120 src=\"cam%03d.jpg\"> ",photo_n-i-1);
-        }else sprintf(s,"<img width=160 height=120 src=\"cam000.jpg\"> ");
-        client.println(s);
-        if( i%3==(PICT_NUM%3) ) client.println("<br><br>");
+            snprintf(html, size,"%s<img width=160 height=120 src=\"cam%03d.jpg\"> ",html,photo_n-i-1);
+        }else snprintf(html, size,"%s<img width=160 height=120 src=\"cam000.jpg\"> ",html);
+        if( i%3==(PICT_NUM%3) ) snprintf(html, size,"%s<br><br>",html);
     }
-    delay(10);
-    if(client.available())return;
-    if(!client.connected())return;
-    if(PICT_NUM%3) client.println("<br><br>");
-    client.print("画像サイズ = 約 ");
-    client.print(size/1000);
-    client.print(" KBytes, 保存数 = ");
-    client.print(photo_n);
-    client.print(" 枚, 更新間隔 = ");
-    client.print(update);
-    client.println(" 秒<br><br>");
-    client.println("<hr>");
-    client.println("<h3>HTTP GET</h3>");
-    client.print("<p>http://");
-    client.print(s_ip);
-    if(photo_n>0) sprintf(s,"/cam%03d.jpg",photo_n-1);
-    else sprintf(s,"/cam000.jpg");
-    client.print(s);
-    client.println("</p>");
-    delay(10);
-    if(client.available())return;
-    if(!client.connected())return;
-    sprintf(s,"<form method=\"GET\" action=\"http://%s/\">",s_ip);
-    client.println(s);
-    client.println("<input type=\"submit\" value=\"画面の更新\">");
-    client.println("　<input type=\"submit\" name=\"SHOT\" value=\"写真撮影\"><br><br>");
-    client.println("自動更新:<input type=\"submit\" name=\"INT\" value=\"0 停止\">");
-    client.println("<input type=\"submit\" name=\"INT\" value=\"60 秒(1分)\">");
-    client.println("<input type=\"submit\" name=\"INT\" value=\"120 秒(3分)\">");
-    client.println("<input type=\"submit\" name=\"INT\" value=\"300 秒(5分)\">");
-    client.println("<input type=\"submit\" name=\"INT\" value=\"600 秒(10分)\"><br><br>");
-    client.println("SPIFFS:<input type=\"submit\" name=\"FORMAT\" value=\"初期化\"><br><br>");
-    client.println("</form>");
-    client.println("</body>");
-    client.println("</html>");
+    snprintf(html, size,"%s\
+    画像サイズ = %d Bytes, 保存数 = %d 枚, 更新間隔 = %d 秒<br><br><hr>\
+    <h3>HTTP GET</h3><p>http://%s/cam%03d.jpg</p>\
+    <form method=\"GET\" action=\"http://%s/\"><input type=\"submit\" value=\"画面の更新\">\
+    　<input type=\"submit\" name=\"SHOT\" value=\"写真撮影\"><br><br>\
+    自動更新:<input type=\"submit\" name=\"INT\" value=\"20 秒\">\
+    <input type=\"submit\" name=\"INT\" value=\"60 秒(1分)\">\
+    <input type=\"submit\" name=\"INT\" value=\"120 秒(3分)\">\
+    <input type=\"submit\" name=\"INT\" value=\"300 秒(5分)\">\
+    <input type=\"submit\" name=\"INT\" value=\"600 秒(10分)\"><br><br>\
+    SPIFFS:<input type=\"submit\" name=\"FORMAT\" value=\"初期化\"><br><br>\
+    </form>\n</body>\n</html>\n\
+    ",html,photo_size,photo_n,update,s_ip,photo_n-1,s_ip);
 }
 
-void htmlMesg(WiFiClient &client, char *txt, uint32_t ip){
-    char s_ip[16];
-    
-    sprintf(s_ip,"%i.%i.%i.%i",
-        ip & 255,
-        ip>>8 & 255,
-        ip>>16 & 255,
-        ip>>24
-    );
-    client.println("HTTP/1.0 200 OK");              // HTTP OKを応答
-    client.println("Content-Type: text/html");      // HTMLコンテンツ
-    client.println("Connection: close");            // 応答終了後にセッションを閉じる
-    client.println();
-    client.println("<html>");
-    client.println("<head><title>Test Page</title>");
-    client.println("<meta http-equiv=\"Content-type\" content=\"text/html; charset=UTF-8\">");
-    client.print("<meta http-equiv=\"refresh\" content=\"3;URL=http://");
-    client.print(s_ip);
-    client.println("/\">");
-    client.print("<p>");
-    client.print(txt);
-    client.println("</p>");
-    client.println("</body>");
-    client.println("</html>");
+void sendError404(){
+    String message = "File Not Found\n\n";
+    message += "URI: ";
+    message += server.uri();
+    message += "\nMethod: ";
+    message += ( server.method() == HTTP_GET ) ? "GET" : "POST";
+    message += "\nArguments: ";
+    message += server.args();
+    message += "\n";
+    for ( uint8_t i = 0; i < server.args(); i++ ) {
+        message += " " + server.argName ( i ) + ": " + server.arg ( i ) + "\n";
+    }
+    server.send ( 404, "text/plain", message );
+    Serial.println("Done handleNotFound");
 }
 
+/*
+void getImgFile(int index) {
+    char filename[]="/cam***.jpg";          // 画像ファイル名(ダウンロード用)
+    int i=photo_n-index-1;
+    if(i<0)i=0;
+    sprintf(filename,"/cam%03d.jpg",i);
+    Serial.print("SEND ");
+    Serial.print(filename);                 // ファイル名を液晶へ出力表示
+    file = SPIFFS.open(filename,"r");       // 読み込みのためにファイルを開く
+    if(file){                               // ファイルを開けることが出来た時、
+        server.streamFile(file,"image/jpeg");   // ファイル送信
+        file.close();                       // ファイルを閉じる
+    }else{
+        handleNotFound();
+    }
+    Serial.println(" Done");
+}
+*/
