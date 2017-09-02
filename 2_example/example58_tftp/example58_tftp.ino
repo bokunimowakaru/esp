91bbf4762cc2dadd009ab266dfadb218d22edea5
Wataru KUNINO <xbee@dream.jp>
2017-09-02 11:19:15 +0900

    TFTPによるESPの遠隔設定サンプル

--- 2_example/example58_tftp/example58_tftp.ino
+++ 2_example/example58_tftp/example58_tftp.ino
@@ -1,12 +1,14 @@
 /*******************************************************************************
 Example 58(=32+26): センサデバイス用 TFTPクライアント 設定
 
-本ESPモジュールは、TFTPサーバ上の設定ファイルをダウンロードし、モジュール内の
-設定を変更します。
+                                           Copyright (c) 2016-2017 Wataru KUNINO
+                                           
+TFTPサーバ上から設定ファイルをダウンロードし、モジュール内の設定を変更します。
+本サンプルではADCの入力ピンとディープスリープ時間を設定することが出来ます。
 
-※本サンプル作成の段階では、(暫定的に)下記ライブラリを使用しました。
+※(暫定的に)下記ライブラリを使用しました。
     https://github.com/copercini/arduino-esp32-SPIFFS
-※今後、esp-idfや上記ライブラリを基にしたものが公式サポートされると思います。
+　今後、esp-idfや上記ライブラリを基にしたものが公式サポートされると思います。
 
 Raspberry PiへのTFTPサーバのインストール方法
     $ sudo apt-get install tftpd-hpa
@@ -18,46 +20,51 @@ Raspberry PiへのTFTPサーバのインストール方法
     TFTP_ADDRESS="0.0.0.0:69"
 
 TFTPサーバの起動と停止
-    /etc/init.d/tftpd-hpa start
-    /etc/init.d/tftpd-hpa stop
+    $ chmod 755 /srv/tftp
+    $ sudo /etc/init.d/tftpd-hpa start
+    $ sudo /etc/init.d/tftpd-hpa stop
 
 転送用のファイルを保存
-    $ echo "Hello! This is from RasPi" > /srv/tftp/esp8266_tftpc_1.ini
+    $ echo "; Hello! This is from RasPi" > /srv/tftp/tftpc_1.ini
+    $ echo "ADC_PIN=32" >> /srv/tftp/tftpc_1.ini
+    $ echo "SLEEP_SEC=50" >> /srv/tftp/tftpc_1.ini
+    $ chmod 644 /srv/tftp/tftpc_1.ini
+    $ cat /srv/tftp/tftpc_1.ini
+    ; Hello! This is from RasPi
+    ADC_PIN=32
+    SLEEP_SEC=50
 
 【注意事項】
-・ESPモジュールのTFTPクライアントを起動したままにすると、危険です
-・TFTPサーバ起動したままにすると、Raspberry Piが脅威にさらされます。
-・ウィルスやワームが侵入すると、同じネットワーク上の全ての機器へ感染する恐れが
-　高まります。
-
-                                           Copyright (c) 2016-2017 Wataru KUNINO
+・TFTPクライアント(ESP側)やTFTPサーバ(PCやRaspberry Pi側)起動すると、各機器が
+　セキュリティの脅威にさらされた状態となります。
+・また、ウィルスやワームが侵入すると、同じネットワーク上の全ての機器へ感染する
+　恐れが高まります。
+・インターネットに接続すると外部からの侵入される場合があります。
+・TFTPクライアントは少なくともローカルネット内のみで動作させるようにして下さい。
+・TFTPが不必要なときは、極力、停止させてください。
 *******************************************************************************/
 #include <WiFi.h>                           // ESP32用WiFiライブラリ
 #include <WiFiUdp.h>                        // UDP通信を行うライブラリ
 #include "esp_deep_sleep.h"                 // ESP32用Deep Sleep ライブラリ
 #define PIN_EN 2                            // GPIO 2(24番ピン)をセンサの電源に
-#define PIN_AIN 34                          // GPIO 34 ADC1_CH6(6番ピン)をADCに
+int     PIN_AIN=34;                         // GPIO 34 ADC1_CH6(6番ピン)をADCに
 #define SSID "1234ABCD"                     // 無線LANアクセスポイントのSSID
 #define PASS "password"                     // パスワード
 #define SENDTO "192.168.0.255"              // 送信先のIPアドレス
 #define PORT 1024                           // 送信のポート番号
-#define SLEEP_P 50*1000000                  // スリープ時間 50秒(uint32_t)
+uint32_t SLEEP_P=10*1000000;                // スリープ時間 10秒(uint32_t)
 #define DEVICE "tftpc_1,"                   // デバイス名(5文字+"_"+番号+",")
-#define TFTP_PORT_C 69                      // TFTP接続用ポート番号(既定)
-#define TFTP_PORT_T 1234                    // TFTP転送用ポート番号(任意)
-#define TFTP_TIMEOUT 10                     // TFTP待ち受け時間(ms)
-#define TFTP_FILENAME "/srv/tftp/esp8266_tftpc_1.ini" // TFTP受信ファイル名
-char data[512];                             // TFTPデータ用変数
-WiFiUDP tftp;                               // TFTP通信用のインスタンスを定義
 
 void setup(){                               // 起動時に一度だけ実行する関数
-    pinMode(PIN_AIN,INPUT);                 // アナログ入力の設定
+    char data[512];                         // TFTPデータ用変数
+    int len_tftp;                           // TFTPデータ長
+    
     pinMode(PIN_EN,OUTPUT);                 // センサ用の電源を出力に
     Serial.begin(115200);                   // 動作確認のためのシリアル出力開始
     Serial.println("ESP32 eg.26 TFTP");     // 「ESP32 eg.26」をシリアル出力表示
     WiFi.mode(WIFI_STA);                    // 無線LANをSTAモードに設定
-    delay(10);                              // ESP32に必要な待ち時間
     WiFi.begin(SSID,PASS);                  // 無線LANアクセスポイントへ接続
+    if(ini_init(data)) initialize(data);    // SPIFSSからINIファイルの読み込み
     while(WiFi.status() != WL_CONNECTED){   // 接続に成功するまで待つ
         delay(500);                         // 待ち時間処理
         digitalWrite(PIN_EN,!digitalRead(PIN_EN));      // LEDの点滅
@@ -65,57 +72,31 @@ void setup(){                               // 起動時に一度だけ実行す
     }
     Serial.println(WiFi.localIP());         // 本機のIPアドレスをシリアル出力
     tftpStart();                            // TFTPの開始
-    while( tftpGet() );                     // データ受信
+    do{
+        len_tftp = tftpGet(data);           // TFTP受信(data=受信データ)
+        if(len_tftp>0){
+            initialize(data);               // INIファイル内の内容を変数へ代入
+            ini_save(data);                 // INIファイルをSPIFSSへ書込み
+        }
+    }while(len_tftp);
+    pinMode(PIN_AIN,INPUT);                 // アナログ入力の設定
 }
 
-void tftpStart(){
-    tftp.begin(TFTP_PORT_T);                // TFTP(受信)の開始
-    tftp.beginPacket(SENDTO, TFTP_PORT_C);  // TFTP送信先を設定
-    tftp.write(0x0); tftp.write(0x01);      // Read Requestコマンド(RRQ)
-    tftp.print(TFTP_FILENAME);              // ファイル名
-    tftp.write(0x0);                        // ファイル名の終端
-    tftp.print("netascii");                 // ASCIIモード
-    tftp.write(0x0);                        // モード名の終端
-    tftp.endPacket();                       // TFTP送信の終了(実際に送信する)
-    Serial.println("Send TFTP RRQ");        // 送信完了をシリアル端末へ表示
+void initialize(char *data){
+    /* INIファイル中のADC_PINの値をPIN_AINへ代入する */
+    PIN_AIN=ini_parse(data,"ADC_PIN");
+    if( PIN_AIN<0 || PIN_AIN==1 || PIN_AIN==3 || PIN_AIN>39
+        || (PIN_AIN>4 && PIN_AIN<12)
+        || (PIN_AIN>14 && PIN_AIN<25)
+        || (PIN_AIN>27 && PIN_AIN<32)
+        || (PIN_AIN>36 && PIN_AIN<39)
+    ) PIN_AIN=34;
+    /* INIファイル中のSLEEP_SECの値をSLEEP_Pへ代入する */
+    SLEEP_P=(uint32_t)ini_parse(data,"SLEEP_SEC") * 1000000;
+    if(SLEEP_P < 10 * 1000000) SLEEP_P=10 * 1000000;
 }
 
-int tftpGet(){
-    int len=0,time=0;
-    IPAddress ip;
-    
-    while(len<5){                           // 未受信の間、繰り返し実行
-        delay(1);                           // 1msの待ち時間
-        len = tftp.parsePacket();           // 受信パケット長を変数lenに代入
-        time++;                             // 時間のカウント
-        if(time>TFTP_TIMEOUT) return 0;     // タイムアウト(応答値は0)
-    }
-    ip=tftp.remoteIP();                     // サーバのIPアドレスを取得
-    tftp.read(data, 511);                   // 最大511バイトまで受信する
-    Serial.print("Recieved (");
-    for(int i=0;i<4;i++) Serial.print(data[i],DEC); // コマンド、ブロック番号
-    Serial.print(") ");
-    Serial.print(len - 4);                  // 受信データ長を表示
-    Serial.print(" Bytes from ");
-    Serial.println(ip);                     // サーバのIPアドレスを表示
-    if(len > 511){
-        Serial.println("FILE SIZE ERROR");  // 複数ブロックの転送には対応しない
-        return -1;                          // ERROR(応答値は-1)
-    }
-    if(data[0]==0x0 && data[1]==0x3){       // TFTP転送データの時
-        Serial.print(&data[4]);             // TFTP受信データを表示する
-        Serial.println("[EOF]");
-        tftp.beginPacket(ip, TFTP_PORT_T);  // TFTP ACK送信先を設定
-        tftp.write(0x0); tftp.write(0x04);  // 受信成功コマンド(ACK)を送信
-        tftp.write(data[2]);                // ブロック番号の上位1バイトを送信
-        tftp.write(data[3]);                // ブロック番号の下位1バイトを送信
-        tftp.endPacket();                   // TFTP送信の終了(実際に送信する)
-        return len;
-    }
-    return -1;                              // ERROR(応答値は-1)
-}
-
-void loop() {
+void loop(){
     WiFiUDP udp;                            // UDP通信用のインスタンスを定義
     int adc;                                // 整数型変数adcを定義
     
@@ -126,12 +107,14 @@ void loop() {
     udp.beginPacket(SENDTO, PORT);          // UDP送信先を設定
     udp.print(DEVICE);                      // デバイス名を送信
     udp.println(adc);                       // 変数adcの値を送信
+    Serial.print("ADC=");                   // シリアル出力表示
     Serial.println(adc);                    // シリアル出力表示
     udp.endPacket();                        // UDP送信の終了(実際に送信する)
     delay(200);                             // 送信待ち時間
     esp_deep_sleep(SLEEP_P);                // Deep Sleepモードへ移行
 }
 
+/* ADC入力用スケッチ */
 float mvAnalogIn(uint8_t PIN){
     int in0,in3;
     float ad0,ad3;
