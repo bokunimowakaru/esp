872b5918b350154c628ea5ed7c1f5a51245a3d32
Wataru KUNINO <xbee@dream.jp>
2017-09-09 18:48:18 +0900

    バグ修正

--- 2_example/example27_env/bme280.ino
+++ 2_example/example27_env/bme280.ino
@@ -1,5 +1,9 @@
 /*******************************************************************************
-Arduino用 I2C 温湿度気圧センサ Bosch BME280 raspi_bme280
+Raspberry Pi用 Arduino用 ESP8266用 ESP32用
+I2C 温湿度気圧センサ Bosch BME280 (BMP280) raspi_bme280
+
+BME280 温度 湿度 気圧
+BMP280 温度 気圧
 
 本ソースリストおよびソフトウェアは、ライセンスフリーです。(詳細は別記)
 利用、編集、再配布等が自由に行えますが、著作権表示の改変は禁止します。
@@ -21,14 +25,14 @@ I2C接続のセンサから測定値を取得する
 // connection it to VDDIO results in slave address 1110111 (0x77), 
 // which is the same as BMP280’s I²C address.
 
-/*
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "../libs/soft_i2c.h"
-*/
-typedef unsigned char byte; 
-byte I2C_bme280=0x76;
+#ifndef ARDUINO
+	#include <stdio.h>
+	#include <stdlib.h>
+	#include <string.h>
+	#include "../libs/soft_i2c.h"
+#endif
+typedef uint8_t byte; 
+uint8_t I2C_bme280=0x76;
 
 // #define DEBUG
 
@@ -58,12 +62,12 @@ s16 dig_P7;/**<calibration P7 data*/
 s16 dig_P8;/**<calibration P8 data*/
 s16 dig_P9;/**<calibration P9 data*/
 
-u8  dig_H1;/**<calibration H1 data*/
+u8	dig_H1;/**<calibration H1 data*/
 s16 dig_H2;/**<calibration H2 data*/
-u8  dig_H3;/**<calibration H3 data*/
+u8	dig_H3;/**<calibration H3 data*/
 s16 dig_H4;/**<calibration H4 data*/
 s16 dig_H5;/**<calibration H5 data*/
-s8  dig_H6;/**<calibration H6 data*/
+s8	dig_H6;/**<calibration H6 data*/
 
 s32 t_fine;/**<calibration T_FINE data*/
 	
@@ -114,22 +118,37 @@ uint32_t bme280_compensate_H_int32(int32_t adc_H){
 }
 
 int _bme280_setByte(byte reg, byte data){
-	Wire.beginTransmission(I2C_bme280);
-	Wire.write(reg);
-	Wire.write(data);
-	return (int)(Wire.endTransmission());
+	#ifdef ARDUINO
+		Wire.beginTransmission(I2C_bme280);
+		Wire.write(reg);
+		Wire.write(data);
+		return (int)(Wire.endTransmission());
+	#else
+		byte config[2];
+		config[0]=reg;
+		config[1]=data;
+		return !i2c_write(I2C_bme280,config,2);
+	#endif
 }
 
-int _bme280_getReg(byte reg){
-	Wire.beginTransmission(I2C_bme280);
-	Wire.write(reg);
-	if( Wire.endTransmission()==0){
+uint16_t _bme280_getReg(byte reg){
+	#ifdef ARDUINO
+		Wire.beginTransmission(I2C_bme280);
+		Wire.write(reg);
+		if( Wire.endTransmission()==0){
+			delay(1);
+			Wire.requestFrom((int)I2C_bme280,(int)1);
+			if(Wire.available()==0) return -2;
+			return Wire.read();
+		}
+		return -1;
+	#else
+		byte data;
+		i2c_write(I2C_bme280,&reg,1);	   // 書込みの実行
 		delay(1);
-		Wire.requestFrom(I2C_bme280,0x1);
-		if(Wire.available()==0) return -2;
-		return Wire.read();
-	}
-	return -1;
+		i2c_read(I2C_bme280,&data,1);	   // 読み出し
+		return (int)data;
+	#endif
 }
 
 void _bme280_cal(){
@@ -142,7 +161,7 @@ void _bme280_cal(){
 	dig_P4 = (s16)(_bme280_getReg(0x94) + (_bme280_getReg(0x95)<<8));
 	dig_P5 = (s16)(_bme280_getReg(0x96) + (_bme280_getReg(0x97)<<8));
 	dig_P6 = (s16)(_bme280_getReg(0x98) + (_bme280_getReg(0x99)<<8));
-	dig_P7 = (s16)(_bme280_getReg(0x9A) + (_bme280_getReg(0x9E)<<8));
+	dig_P7 = (s16)(_bme280_getReg(0x9A) + (_bme280_getReg(0x9B)<<8));
 	dig_P8 = (s16)(_bme280_getReg(0x9C) + (_bme280_getReg(0x9D)<<8));
 	dig_P9 = (s16)(_bme280_getReg(0x9E) + (_bme280_getReg(0x9F)<<8));
 	dig_H1 = (u8)(_bme280_getReg(0xA1));
@@ -154,136 +173,165 @@ void _bme280_cal(){
 }
 
 float bme280_getTemp(){
-    int32_t in;
-    in = _bme280_getReg(0xFA);            		// temp_msb[7:0]
-    in <<= 8;
-    in |= _bme280_getReg(0xFB);             	// temp_lsb[7:0]
-    in <<= 4;
-    in |= _bme280_getReg(0xFC); 				// temp_xlsb[3:0]
-//    printf("getTemp  %08X %d\n",in,in);
+	int32_t in;
+	in = _bme280_getReg(0xFA);			  		// temp_msb[7:0]
+	in <<= 8;
+	in |= _bme280_getReg(0xFB); 				// temp_lsb[7:0]
+	in <<= 4;
+	in |= _bme280_getReg(0xFC); 				// temp_xlsb[3:0]
+//	  printf("getTemp  %08X %d\n",in,in);
 	return ((float)BME280_compensate_T_int32(in))/100.;
 }
 
 
 float bme280_getHum(){
-    int32_t in;
-    in = _bme280_getReg(0xFD);            		// hum_msb[7:0]
-    in <<= 8;
-    in |= _bme280_getReg(0xFE);             	// hum_lsb[7:0]
-//  printf("getHum   %08X\n",in);
+	int32_t in;
+	in = _bme280_getReg(0xFD);			  		// hum_msb[7:0]
+	in <<= 8;
+	in |= _bme280_getReg(0xFE); 				// hum_lsb[7:0]
+//	printf("getHum   %08X\n",in);
 	return ((float)bme280_compensate_H_int32(in))/1024.;
 }
 
 float bme280_getPress(){
-    int32_t in;
-    in = _bme280_getReg(0xF7);      			// press_msb[7:0]
-    in <<= 8;
-    in |= _bme280_getReg(0xF8);      			// press_lsb[7:0]
-    in <<= 4;
-    in |= _bme280_getReg(0xF9);				// press_xlsb[3:0]
-//  printf("getPress %08X\n",in);
+	int32_t in;
+	in = _bme280_getReg(0xF7);					// press_msb[7:0]
+	in <<= 8;
+	in |= _bme280_getReg(0xF8); 	 			// press_lsb[7:0]
+	in <<= 4;
+	in |= _bme280_getReg(0xF9);				// press_xlsb[3:0]
+//	printf("getPress %08X\n",in);
 	return ((float)BME280_compensate_P_int64(in))/25600.;
 }
 
 int bme280_init(){
-    byte reg,data,in;
-    int i;
-    
-    /*
-    i2c_init();
-    */
-    Wire.begin();
-    
-    _bme280_cal();
-    
-    reg= 0xF5;                 	// config
-//  data=0b11000000;
-    data=0b11000000;
-    //     | || | |___________________ 触るな SCI切換え
-    //     | ||_|_____________________ filter[2:0]
-    //     |_|________________________ t_sb[2:0]
-    if(_bme280_setByte(reg,data)){		// 書込みの実行
-		Serial.println("ERROR(11): i2c writing config reg");
+	byte reg,data,in;
+	int i;
+	
+	#ifdef ARDUINO
+		Wire.begin();
+	#else
+		i2c_init();
+	#endif
+	
+	_bme280_cal();
+	
+	reg= 0xF5;				   	// config
+//	data=0b11000000;
+	data=0b00000000;
+	//	   | || | |___________________ 触るな SCI切換え
+	//	   | ||_|_____________________ filter[2:0]
+	//	   |_|________________________ t_sb[2:0]
+	if(_bme280_setByte(reg,data)){		// 書込みの実行
+		#ifdef ARDUINO
+			Serial.println("ERROR(11): i2c writing config reg");
+		#else
+			fprintf(stderr,"ERROR(11): i2c writing config reg\n");
+		#endif
 		return 11;
-    }
-    
-    reg= 0xF2;                 	// trl_hum
-    data=0b00000001;
-    //          |_|___________________ osrs_h[2:0]
-    if(_bme280_setByte(reg,data)){		// 書込みの実行
-		Serial.println("ERROR(12): i2c writing trl_hum reg");
-		return 12;
-    }
-    
-    reg= 0xF4;                 	// ctrl_meas
-    data=0b00100111;
-    //     | || |||___________________ mode[1:0]
-    //     | ||_|_____________________ osrs_p[2:0]
-    //     |_|________________________ osrs_t[2:0]
-    if(_bme280_setByte(reg,data)){		// 書込みの実行
-		Serial.println("ERROR(13): i2c writing ctrl_meas reg");
-		return 13;
-    }    
-    in=_bme280_getReg(0xD0);
-	if(in != 0x58 && in != 0x60){
-    //  fprintf(stderr,"ERROR(21):  chip_id (%02X)\n",in);
-		Serial.print("ERROR(21):  chip_id = 0x");
-		Serial.println(in,HEX);
-    	return 21;
 	}
-    for(i=0;i<50;i++){
+	
+	reg= 0xF2;				   	// trl_hum
+	data=0b00000001;
+	//			|_|___________________ osrs_h[2:0]
+	if(_bme280_setByte(reg,data)){		// 書込みの実行
+		#ifdef ARDUINO
+			Serial.println("ERROR(12): i2c writing trl_hum reg");
+		#else
+			fprintf(stderr,"ERROR(12): i2c writing trl_hum reg\n");
+		#endif
+		return 12;
+	}
+	
+	reg= 0xF4;				   	// ctrl_meas
+	data=0b00100111;
+	//	   | || |||___________________ mode[1:0]
+	//	   | ||_|_____________________ osrs_p[2:0]
+	//	   |_|________________________ osrs_t[2:0]
+	if(_bme280_setByte(reg,data)){		// 書込みの実行
+		#ifdef ARDUINO
+			Serial.println("ERROR(13): i2c writing ctrl_meas reg");
+		#else
+			fprintf(stderr,"ERROR(13): i2c writing ctrl_meas reg\n");
+		#endif
+		return 13;
+	}	 
+	in=_bme280_getReg(0xD0);
+	if(in != 0x58 && in != 0x60){
+		#ifdef ARDUINO
+			Serial.print("ERROR(21):  chip_id = 0x");
+			Serial.println(in,HEX);
+		#else
+			fprintf(stderr,"ERROR(21):  chip_id (%02X)\n",in);
+		#endif
+		return 21;
+	}
+	/*
+	for(i=0;i<50;i++){
 		in=_bme280_getReg(0xF3);
 		#ifdef DEBUG
-			/*
-			printf("getReg   %02X\n",in);
-			*/
-			Serial.print("getReg 0x");
-			Serial.println(in,HEX);
+			#ifdef ARDUINO
+				Serial.print("getReg 0x");
+				Serial.println(in,HEX);
+			#else
+				printf("getReg   %02X\n",in);
+			#endif
 		#endif
 		if((in&0x04)==0) break;
 		delay(20);
 	}
 	if(i==50){
-		Serial.println("ERROR(31): failed to read results");
-    	return 31;
+		#ifdef ARDUINO
+			Serial.println("ERROR(31): failed to read results");
+		#else
+			fprintf(stderr,"ERROR(31): failed to read results\n");
+		#endif
+		return 31;
 	}
+	*/
+	delay(20);
 	return 0;
 }
 
 int bme280_stop(){
-    byte reg,data;
-    reg= 0xF4;                 	// ctrl_meas
-    data=0x00;
-    _bme280_setByte(reg,data);  // 書込みの実行
-//  i2c_close();
+	byte reg,data;
+	int ret;
+	reg= 0xF4;					   	// ctrl_meas
+	data=0x00;
+	ret=_bme280_setByte(reg,data);	// 書込みの実行
+	#ifndef ARDUINO
+		i2c_close();
+	#endif
+	return ret;
 }
 
 void bme280_print(float temp, float hum, float press){
-	/*
-    printf("%3.2f ",temp);
-    printf("%3.2f ",hum);
-    printf("%4.2f\n",press);
-    */
-    Serial.print("Temp ="); Serial.println(temp,2);
-    Serial.print("Humi ="); Serial.println(hum,2);
-    Serial.print("Press="); Serial.println(press,2);
+	#ifdef ARDUINO
+		Serial.print("Temp ="); Serial.println(temp,2);
+		Serial.print("Humi ="); Serial.println(hum,2);
+		Serial.print("Press="); Serial.println(press,2);
+	#else
+		printf("%3.2f ",temp);
+		printf("%3.2f ",hum);
+		printf("%4.2f\n",press);
+	#endif
 }
 
-/*
+#ifndef ARDUINO
 int main(int argc,char **argv){
-    if( argc == 2 ) I2C_bme280=(byte)strtol(argv[1],NULL,16);
-    if( I2C_bme280>=0x80 ) I2C_bme280>>=1;
-    if( argc < 1 || argc > 2 ){
-        fprintf(stderr,"usage: %s [I2C_bme280]\n",argv[0]);
-        return -1;
-    }
-    #ifdef DEBUG
-        printf("I2C_bme280 =0x%02X\n",I2C_bme280);
-    #endif
+	if( argc == 2 ) I2C_bme280=(byte)strtol(argv[1],NULL,16);
+	if( I2C_bme280>=0x80 ) I2C_bme280>>=1;
+	if( argc < 1 || argc > 2 ){
+		fprintf(stderr,"usage: %s [I2C_bme280]\n",argv[0]);
+		return -1;
+	}
+	#ifdef DEBUG
+		printf("I2C_bme280 =0x%02X\n",I2C_bme280);
+	#endif
 
 	bme280_init();
 	bme280_print(bme280_getTemp(),bme280_getHum(),bme280_getPress());
 	bme280_stop();
-    return 0;
+	return 0;
 }
-*/
+#endif
--- 2_example/example27_env/ccs811.ino
+++ 2_example/example27_env/ccs811.ino
@@ -1,8 +1,9 @@
 /*******************************************************************************
-Arduino 用 I2C 揮発性有機化合物センサ AMS CCS811 ccs811.ino
+Raspberry Pi用 Arduino用 ESP8266用 ESP32用
+I2C 揮発性有機化合物センサ AMS CCS811 raspi_ccs811.c ccs811.ino
 
-                                        Copyright (c) 2016-2017 Wataru KUNINO
-                                        http://www.geocities.jp/bokunimowakaru/
+										Copyright (c) 2016-2017 Wataru KUNINO
+										http://www.geocities.jp/bokunimowakaru/
 ********************************************************************************
 
 本ソースリストおよびソフトウェアは、ライセンスフリーです。(詳細は別記)
@@ -12,8 +13,8 @@ I2C接続のセンサから測定値を取得する
 AMS CCS811 Carbon Monoxide CO VOCs Air Quality Numerical Gas Sensors
 
 I2Cアドレス設定
-                        0x5A    When ADDR is low
-                        0x5B    When ADDR is high
+						0x5A	When ADDR is low
+						0x5B	When ADDR is high
 
 揮発性有機化合物 VOCガス
 二酸化炭素CO2(相当)
@@ -21,103 +22,110 @@ I2Cアドレス設定
 参考文献：AMS CCS811 データシート Ver 1.00
 *******************************************************************************/
 
-#include <Wire.h> 
-#define I2C_ccs811 0x5A						// I2Cアドレス
+#ifdef ARDUINO
+	#include <Wire.h> 
+#else
+	#include <stdio.h>
+	#include <stdlib.h>
+	#include <string.h>
+	#include "../libs/soft_i2c.h"
+	typedef unsigned char byte;
+#endif
+byte I2C_ccs811=0x5A;						// I2Cアドレス
 
 // #define DEBUG
 
 uint8_t _ccs811_getByte(byte reg){
-	/*
-	uint8_t tx=reg;
-	uint8_t rx;
-	i2c_write(I2C_ccs811,&tx,1);	   		// 書込みの実行
-	delay(10);
-	i2c_read(I2C_ccs811,&rx,1);
-	#ifdef DEBUG
-	//	printf("rx=%02x\n",rx);
-	#endif
-	return rx;
-	*/
 	uint8_t ret=0xFF;
-	Wire.beginTransmission(I2C_ccs811);
-	Wire.write(reg);
-	delay(10);								// 書込み時間
-	if( Wire.endTransmission()==0){
-		Wire.requestFrom(I2C_ccs811,0x1);
-		if(Wire.available()==0) return 0xFE;
-		ret =Wire.read();
-	}
+	#ifdef ARDUINO
+		Wire.beginTransmission(I2C_ccs811);
+		Wire.write(reg);
+		delay(10);								// 書込み時間
+		if( Wire.endTransmission()==0){
+			Wire.requestFrom((int)I2C_ccs811,1);
+			if(Wire.available()==0) return 0xFE;
+			ret =Wire.read();
+		}
+	#else
+		i2c_write(I2C_ccs811,&reg,1);	   		// 書込みの実行
+		delay(10);
+		i2c_read(I2C_ccs811,&ret,1);
+		#ifdef DEBUG
+		//	printf("rx=%02x\n",rx);
+		#endif
+	#endif
 	return ret;
 }
 
 int _ccs811_getReg(byte reg, byte *rx, int len){
-	/*
 	uint8_t tx=reg;
-	int i;
-	if(len < 0 || len>8) return -1;
-	i2c_write(I2C_ccs811,&tx,1);	   		// 書込みの実行
-	delay(10);
-	i=i2c_read(I2C_ccs811,(byte *)rx,len);
-	#ifdef DEBUG
-		printf("rx[%d]=",i);
-		for(i=0;i<len;i++){
-			printf("%02x ",rx[i]);
-		}
-		printf("\n");
-	#endif
-	return i;
-	*/
 	int i=-1;
 	if(len < 0 || len>8) return -1;
-	Wire.beginTransmission(I2C_ccs811);
-	Wire.write(reg);
-	delay(10);
-	if(Wire.endTransmission() == 0){
-		Wire.requestFrom(I2C_ccs811,(byte)len);
-		for(i=0;i<len;i++){
-			if(Wire.available()==0) break;
-			rx[i] =Wire.read();
+	#ifdef ARDUINO
+		Wire.beginTransmission(I2C_ccs811);
+		Wire.write(reg);
+		delay(10);
+		if(Wire.endTransmission() == 0){
+			Wire.requestFrom((int)I2C_ccs811,len);
+			for(i=0;i<len;i++){
+				if(Wire.available()==0) break;
+				rx[i] =Wire.read();
+			}
 		}
-	}
+	#else
+		i2c_write(I2C_ccs811,&tx,1);	   		// 書込みの実行
+		delay(10);
+		i=i2c_read(I2C_ccs811,(byte *)rx,len);
+		#ifdef DEBUG
+			printf("rx[%d]=",i);
+			for(i=0;i<len;i++){
+				printf("%02x ",rx[i]);
+			}
+			printf("\n");
+		#endif
+	#endif
 	return i;
 }
 
 int _ccs811_set0Byte(byte reg){			// 書き込み値の無いコマンド(APP_START=0xF4)
-	/*
-	uint8_t tx=reg;
-	return i2c_write(I2C_ccs811,&tx,1); 	  // 書込みの実行
-	*/
-	Wire.beginTransmission(I2C_ccs811);
-	Wire.write(reg);
-	return (int)(!Wire.endTransmission());
+	#ifdef ARDUINO
+		Wire.beginTransmission(I2C_ccs811);
+		Wire.write(reg);
+		return (int)(!Wire.endTransmission());
+	#else
+		uint8_t tx=reg;
+		return i2c_write(I2C_ccs811,&tx,1); 	  // 書込みの実行
+	#endif
 }
 
 int _ccs811_setByte(byte reg, byte data){
-	/*
-	uint8_t tx[2];
-	tx[0]=reg;
-	tx[1]=data;
-	return i2c_write(I2C_ccs811,tx,2); 	  // 書込みの実行
-	*/
-	Wire.beginTransmission(I2C_ccs811);
-	Wire.write(reg);
-	Wire.write(data);
-	return (int)(!Wire.endTransmission());
+	#ifdef ARDUINO
+		Wire.beginTransmission(I2C_ccs811);
+		Wire.write(reg);
+		Wire.write(data);
+		return (int)(!Wire.endTransmission());
+	#else
+		uint8_t tx[2];
+		tx[0]=reg;
+		tx[1]=data;
+		return i2c_write(I2C_ccs811,tx,2); 	  // 書込みの実行
+	#endif
 }
 
 int _ccs811_setReg(byte reg, byte *data, int len){
-	/*
-	uint8_t tx[9];
-	int i;
-	
-	if(len < 0 || len>8) return -1;
-	for(i=0;i<len;i++)tx[i+1]=data[i];
-	return i2c_write(I2C_ccs811,tx,len+1);	// 書込みの実行
-	*/
-	Wire.beginTransmission(I2C_ccs811);
-	Wire.write(reg);
-	Wire.write(data, len);
-	return (int)(!Wire.endTransmission());
+	#ifdef ARDUINO
+		Wire.beginTransmission(I2C_ccs811);
+		Wire.write(reg);
+		Wire.write(data, len);
+		return (int)(!Wire.endTransmission());
+	#else
+		uint8_t tx[9];
+		int i;
+		if(len < 0 || len>8) return -1;
+		for(i=0;i<len;i++)tx[i+1]=data[i];
+		return i2c_write(I2C_ccs811,tx,len+1);	// 書込みの実行
+	#endif
+
 }
 
 int _ccs811_setAppStart(){
@@ -145,40 +153,64 @@ int ccs811_stop(){
 
 int ccs811_getVals(){
 	uint8_t rx[8];
-	int i,len,adc,co2;
-	char s[80];
+	int len,co2;
 	#ifdef DEBUG
-		int current;
+		int current,adc,i;
 	#endif
 	
 	len=_ccs811_getReg(0x02, rx, 8);		// 0x02 ALG_RESULT_DATA
 	if(len != 8){
-		Serial.print("ERROR: ccs811_getVals, len=0x");
-		Serial.println(len);
+		#ifdef ARDUINO
+			Serial.print("ERROR: ccs811_getVals, len=0x");
+			Serial.println(len);
+		#else
+			fprintf(stderr,"ERROR: ccs811_getVals, len=0x%02X\n",len);
+		#endif
 		return -1;
 	}
-	i=((int)rx[6])*256+(int)rx[7];
-	adc = i & 0x03FF;
 	co2 = ((int)rx[0])*256+(int)rx[1];
 	#ifdef DEBUG
+		i=((int)rx[6])*256+(int)rx[7];
+		adc = i & 0x03FF;
 		current = i>>10;
-		if(len==8){
-			Serial.println("------------------------");
-			Serial.println("RESULT_DATA");
-			Serial.println("------------------------");
-			Serial.print("eCO2        ="); Serial.println( co2 );
-			Serial.print("TVOC        ="); Serial.println( ((int)rx[2])*256+(int)rx[3] );
-			Serial.print("STATUS      =0x"); Serial.println( rx[4],HEX );
-			Serial.print("ERROR_ID    =0x"); Serial.println( rx[5],HEX );
-			Serial.print("Current(uA) ="); Serial.println( current );
-			Serial.print("Raw ADC     ="); Serial.println( adc );
-		}else{
-			for(i=0;i<len;i++){
-				Serial.print(" 0x");
-				Serial.print(rx[i],HEX);
+		#ifdef ARDUINO
+			if(len==8){
+				Serial.println("------------------------");
+				Serial.println("RESULT_DATA");
+				Serial.println("------------------------");
+				Serial.print("eCO2        ="); Serial.println( co2 );
+				Serial.print("TVOC        ="); Serial.println( ((int)rx[2])*256+(int)rx[3] );
+				Serial.print("STATUS      =0x"); Serial.println( rx[4],HEX );
+				Serial.print("ERROR_ID    =0x"); Serial.println( rx[5],HEX );
+				Serial.print("Current(uA) ="); Serial.println( current );
+				Serial.print("Raw ADC     ="); Serial.println( adc );
+			}else{
+				for(i=0;i<len;i++){
+					Serial.print(" 0x");
+					Serial.print(rx[i],HEX);
+				}
+				Serial.println();
 			}
-			Serial.println();
-		}
+		#else
+			if(len==8){
+				printf("------------------------\n");
+				printf("RESULT_DATA\n");
+				printf("------------------------\n");
+				printf("eCO2        =0x%02X%02X (%d)\n",rx[0],rx[1],((int)rx[0])*256+(int)rx[1]);
+				printf("TVOC        =0x%02X%02X (%d)\n",rx[2],rx[3],((int)rx[2])*256+(int)rx[3]);
+				printf("STATUS      =0x%02X\n",rx[4]);
+				printf("ERROR_ID    =0x%02X\n",rx[5]);
+				printf("RAW_DATA    =0x%02X%02X (%d)\n",rx[6],rx[7],i);
+				printf("Current     =0x%02X (%d)\n",current,current);
+				printf("Raw ADC     =0x%04X (%d)\n",adc,adc);
+			}else{
+				printf("rx[%d]=",len);
+				for(i=0;i<len;i++){
+					printf("%02x ",rx[i]);
+				}
+				printf("\n");
+			}
+		#endif
 	#endif
 	return co2;
 }
@@ -203,15 +235,17 @@ int ccs811_getRAW(){
 	i=_ccs811_getReg(0x03, rx, 2);
 	if(i==2) ret=(((int)(rx[0] & 0x03))<<8) | (int)rx[1];
 	#ifdef DEBUG
-		Serial.print("raw={0x");
-		Serial.print(rx[0],HEX);
-		Serial.print(",0x");
-		Serial.print(rx[1],HEX);
-		Serial.print(",");
-		Serial.print((int)(rx[0]>>2));	//	Current Selected (0μA to 63μA).
-		Serial.print("uA, ");
-		Serial.print(ret);	//
-		Serial.print("},");
+		#ifdef ARDUINO
+			Serial.print("raw={0x");
+			Serial.print(rx[0],HEX);
+			Serial.print(",0x");
+			Serial.print(rx[1],HEX);
+			Serial.print(",");
+			Serial.print((int)(rx[0]>>2));	//	Current Selected (0μA to 63μA).
+			Serial.print("uA, ");
+			Serial.print(ret);	//
+			Serial.print("},");
+		#endif
 	#endif
 	return ret;
 }
@@ -222,30 +256,40 @@ int ccs811_getCO2(){						   // 二酸化炭素濃度（ppm)を取得
 	
 	ret=_ccs811_getReg(0x02,rx,6);
 	if(ret != 6){
-		Serial.print("ERROR: getCO2, len=0x");
-		Serial.println(ret);
+		#ifdef ARDUINO
+			Serial.print("ERROR: getCO2, len=0x");
+			Serial.println(ret);
+		#else
+	  		fprintf(stderr,"ERROR: getCO2, len=0x%02X\n",ret);
+		#endif
 		return -1;
 	}
 	#ifdef DEBUG
-		Serial.print("co2={0x");
-		Serial.print(rx[0],HEX);	// eCO2 High Byte
-		Serial.print(",0x");
-		Serial.print(rx[1],HEX);	// eCO2 Low Byte
-		Serial.print("},tv={0x");
-		Serial.print(rx[2],HEX);	// TVOC High Byte
-		Serial.print(",0x");
-		Serial.print(rx[3],HEX);	// TVOC Low Byte
-		Serial.print("},sta={0x");
-		Serial.print(rx[4],HEX);	// STATUS
-		Serial.print(",0x");
-		Serial.print(rx[5],HEX);	// ERROR_ID
-		Serial.print("},");
-		Serial.print(ret);
-		Serial.print(", ");
+		#ifdef ARDUINO
+			Serial.print("co2={0x");
+			Serial.print(rx[0],HEX);	// eCO2 High Byte
+			Serial.print(",0x");
+			Serial.print(rx[1],HEX);	// eCO2 Low Byte
+			Serial.print("},tv={0x");
+			Serial.print(rx[2],HEX);	// TVOC High Byte
+			Serial.print(",0x");
+			Serial.print(rx[3],HEX);	// TVOC Low Byte
+			Serial.print("},sta={0x");
+			Serial.print(rx[4],HEX);	// STATUS
+			Serial.print(",0x");
+			Serial.print(rx[5],HEX);	// ERROR_ID
+			Serial.print("},");
+			Serial.print(ret);
+			Serial.print(", ");
+		#endif
 	#endif
 	if(rx[5] != 0x00){
-		Serial.print("ERROR: getCO2, error code=0x");
-		Serial.println(rx[5],HEX);
+		#ifdef ARDUINO
+			Serial.print("ERROR: getCO2, error code=0x");
+			Serial.println(rx[5],HEX);
+		#else
+	  		fprintf(stderr,"ERROR: getCO2, error code=0x%02X\n",rx[5]);
+		#endif
 		return -1;
 	}
 	return ((int)rx[0])*256+(int)rx[1];
@@ -254,80 +298,151 @@ int ccs811_getCO2(){						   // 二酸化炭素濃度（ppm)を取得
 int ccs811_setup(float temp, float hum){
 	byte ret,id,status,mode;
 	
-//	Wire.begin();
+	#ifdef ARDUINO
+	//	Wire.begin();
+	#else
+		i2c_init();
+	#endif
 	id=_ccs811_getByte(0x20);			// HW_ID
 	#ifdef DEBUG
-		Serial.println();
-		Serial.print("ccs811_setup ");
-		Serial.print(temp);
-		Serial.print(",");
-		Serial.println(hum);
-		Serial.print("HW_ID       =0x"); Serial.println( id,HEX );
+		#ifdef ARDUINO
+			Serial.println();
+			Serial.print("ccs811_setup ");
+			Serial.print(temp);
+			Serial.print(",");
+			Serial.println(hum);
+			Serial.print("HW_ID       =0x"); Serial.println( id,HEX );
+		#else
+			printf("HW_ID       =0x%02X\n",id);
+		#endif
 	#endif
 	if(id != 0x81){
-		Serial.println("ERROR: unknown device ");
+		#ifdef ARDUINO
+			Serial.println("ERROR: unknown device ");
+		#else
+			fprintf(stderr,"ERROR: unknown device (0x%02X)\n",id);
+			printf("-1\n");
+			i2c_close();
+			exit(-1);
+		#endif
 		return -1;
 	}
-	
+	if(temp < -100 || temp > 100) temp=27;
+	if(hum <= 0 || hum > 100) hum=50;
 	ret=ccs811_setEnv(temp, hum);	// 補正用の温度と湿度を設定
 	if(ret==0){
-		Serial.println("ERROR: failed to set env");
+		#ifdef ARDUINO
+			Serial.println("ERROR: failed to set env");
+		#else
+			fprintf(stderr,"ERROR: failed to set env\n");
+		#endif
 		return -1;
 	}
 	
 	ret=_ccs811_setAppStart();
 	#ifdef DEBUG
-		Serial.print("AppStart    ="); Serial.println( ret );
+		#ifdef ARDUINO
+			Serial.print("AppStart    ="); Serial.println( ret );
+		#else
+			printf("AppStart    =%1X\n",ret);
+		#endif
 	#endif
 	if(ret==0){
-		Serial.println("ERROR: failed to start app");
+		#ifdef ARDUINO
+			Serial.println("ERROR: failed to start app");
+		#else
+			fprintf(stderr,"ERROR: failed to start app\n");
+			printf("-1\n");
+			i2c_close();
+			exit(-1);
+		#endif
 		return -1;
 	}
 	
 	status=_ccs811_getByte(0x00);		// STATUS
 	#ifdef DEBUG
-		Serial.println("------------------------");
-		Serial.print("STATUS      =0x"); Serial.println( status, HEX );
-		Serial.println("------------------------");
-		Serial.print("FW_MODE     =0x"); Serial.println( (status & 0x80) >> 7,HEX);
-		Serial.print("APP_VALID   =0x"); Serial.println( (status & 0x10) >> 4,HEX);
-		Serial.print("DATA_READY  =0x"); Serial.println( (status & 0x08) >> 3,HEX);
-		Serial.print("ERROR_FLAG  =0x"); Serial.println( (status & 0x01),HEX);
-		if(status & 0x01){
-			Serial.print(" 0x");
-			Serial.println(_ccs811_getByte(0xE0),HEX);
-		}Serial.println();
+		#ifdef ARDUINO
+			Serial.println("------------------------");
+			Serial.print("STATUS      =0x"); Serial.println( status, HEX );
+			Serial.println("------------------------");
+			Serial.print("FW_MODE     =0x"); Serial.println( (status & 0x80) >> 7,HEX);
+			Serial.print("APP_VALID   =0x"); Serial.println( (status & 0x10) >> 4,HEX);
+			Serial.print("DATA_READY  =0x"); Serial.println( (status & 0x08) >> 3,HEX);
+			Serial.print("ERROR_FLAG  =0x"); Serial.println( (status & 0x01),HEX);
+			if(status & 0x01){
+				Serial.print(" 0x");
+				Serial.println(_ccs811_getByte(0xE0),HEX);
+			}Serial.println();
+		#else
+			printf("------------------------\n");
+			printf("STATUS      =0x%02X\n",status);
+			printf("------------------------\n");
+			printf("FW_MODE     =%1X\n",(status & 0x80) >> 7);
+			printf("APP_VALID   =%1X\n",(status & 0x10) >> 4);
+			printf("DATA_READY  =%1X\n",(status & 0x08) >> 3);
+			printf("ERROR       =%1X",(status & 0x01));
+			if(status & 0x01){
+				printf(" (0x%02X)\n",_ccs811_getByte(0xE0));
+			}else printf("\n");
+		#endif
 	#endif
-	if( (status & 0x80) == 0 || status & 0xFE == 0xFE ){
-		Serial.println("ERROR: failed APP starting");
+	if( (status & 0x80) == 0 || (status & 0xFE) == 0xFE ){
+		#ifdef ARDUINO
+			Serial.println("ERROR: failed APP starting");
+		#else
+			fprintf(stderr,"ERROR: failed APP starting\n");
+			printf("-1\n");
+			i2c_close();
+			exit(-1);
+		#endif
 		return -1;
 	}
 	
 	ret= ccs811_start();
 	#ifdef DEBUG
-		Serial.print("MeasureStart="); Serial.println( ret );
+		#ifdef ARDUINO
+			Serial.print("MeasureStart="); Serial.println( ret );
+		#else
+			printf("MeasureStart=%1X\n",(ret>0));
+		#endif
 	#endif
 	
 	delay(1000);
 	mode=_ccs811_getByte(0x01); 		// MEAS_MODE
 	#ifdef DEBUG
-		Serial.println("------------------------");
-		Serial.print("MEAS_MODE   =0x"); Serial.println( mode, HEX );
-		Serial.println("------------------------");
-		Serial.print("DRIVE_MODE  =0x"); Serial.println( (mode & 0x70) >> 4);
-		Serial.print("INT_DATARDY =0x"); Serial.println( (mode & 0x08) >> 3);
-		Serial.print("INT_THRESH  =0x"); Serial.println( (mode & 0x04) >> 2);
+		#ifdef ARDUINO
+			Serial.println("------------------------");
+			Serial.print("MEAS_MODE   =0x"); Serial.println( mode, HEX );
+			Serial.println("------------------------");
+			Serial.print("DRIVE_MODE  =0x"); Serial.println( (mode & 0x70) >> 4);
+			Serial.print("INT_DATARDY =0x"); Serial.println( (mode & 0x08) >> 3);
+			Serial.print("INT_THRESH  =0x"); Serial.println( (mode & 0x04) >> 2);
+		#else
+			printf("------------------------\n");
+			printf("MEAS_MODE   =0x%02X\n",mode);
+			printf("------------------------\n");
+			printf("DRIVE_MODE  =%1X\n",(mode & 0x70) >> 4);
+			printf("INT_DATARDY =%1X\n",(mode & 0x08) >> 3);
+			printf("INT_THRESH  =%1X\n",(mode & 0x04) >> 2);
+		#endif
 	#endif
 	if((mode & 0x70) == 0x00){
-		Serial.print("ERROR: failed to set mode");
+		#ifdef ARDUINO
+			Serial.print("ERROR: failed to set mode");
+		#else
+			fprintf(stderr,"ERROR: failed to set mode (%d)\n",(mode & 0x70)>>4);
+			printf("-1\n");
+			i2c_close();
+			exit(-1);
+		#endif
 		return -1;
 	}
 	return 0;
 }
 
-/*
-int ccs811_main(int argc,char **argv){
-	int co2=0;
+#ifndef ARDUINO
+int main(int argc,char **argv){
+	int co2=0,prev= 9999,time=0;
 	
 	if( argc == 2 ) I2C_ccs811=(byte)strtol(argv[1],NULL,16);
 	if( I2C_ccs811>=0x80 ) I2C_ccs811>>=1;
@@ -339,17 +454,25 @@ int ccs811_main(int argc,char **argv){
 		printf("I2C_ccs811 =0x%02X\n",I2C_ccs811);
 	#endif
 
-	ccs811_setup();
-	while(co2==0){
+	ccs811_setup(27,50);	// 温度27℃、湿度50％
+	do{
+		if(co2<0){							// CO2センサのI2Cの通信異常時
+			fprintf(stderr,"I2C hardware is locked. Please Reset RST pin.\n");
+			i2c_close();
+			ccs811_setup(27,50);
+		}
+		if(time > 10) break;
+		time++;
+		delay(1010);						// CO2測定間隔が1秒なので1.01待ち
+		if(co2>400)prev=co2;				// 前回値を保存
 		co2=ccs811_getCO2();
-		if(co2>0) break;
+		fprintf(stderr,"CO2 = %d\n",co2);
 		#ifdef DEBUG
 		if(co2==0) co2=ccs811_getVals();
 		#endif
-		delay(1000);
-	}
+	}while(co2<=400 || (abs(co2-prev)+1)*100/co2 > 5);
 	printf("%d\n",co2);
 	i2c_close();
 	return 0;
 }
-*/
+#endif
