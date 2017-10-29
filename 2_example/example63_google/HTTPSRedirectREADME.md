8bb5906dc17de616061ace423dc59fbfc335e186
Wataru KUNINO <xbee@dream.jp>
2017-10-29 12:53:54 +0900

    Googleカレンダから予定を取得し、液晶へ表示する

---
+++ 2_example/example63_google/HTTPSRedirectREADME.md
@@ -0,0 +1,25 @@
+# ESP8266-Google-Calendar-Arduino
+Interaction between an ESP8266 and the Google Calendar.
+
+Instructions:
+Add the googlescript to your google account, set your calendar and publish it.
+
+Copy the id and paste it in the arduino file.
+
+Set your wifi settings.
+
+*The code is not the nicest - but it works :)
+
+/*  HTTPS with follow-redirect
+ *  Created by Sujay S. Phadke, 2016
+ *  All rights reserved.
+ *  
+ *  Modified by Daniel Willi, 2016
+
+        Google Calendar Integration ESP8266
+        Created by Daniel Willi, 2016
+        https://github.com/wilda17/ESP8266-Google-Calendar-Arduino
+
+ *  Modified for ESP32
+ *  by Wataru KUNINO, 2017
+ */
---
+++ 2_example/example63_google/utf_del_uni.ino
@@ -0,0 +1,38 @@
+/*********************************************************************
+本ソースリストおよびソフトウェアは、ライセンスフリーです。(詳細は別記)
+利用、編集、再配布等が自由に行えますが、著作権表示の改変は禁止します。
+
+UTF8の制御コードを除去します(UTF8をASCIIカナ文字へ変換するときに使用)
+
+                               Copyright (c) 2014-2017 Wataru KUNINO
+                               http://www.geocities.jp/bokunimowakaru/
+*********************************************************************/
+
+char _utf_del_uni_buf[65];
+
+char * utf_del_uni(char *in){
+	char *s=_utf_del_uni_buf;
+	unsigned int i=0;
+	unsigned int j=0;
+	
+	strncpy(s,in,64);
+	s[64]=0;
+	while(s[i]!='\0'){
+		if((byte)s[i]==0xEF){
+			if((byte)s[i+1]==0xBE) s[i+2] += 0x40;
+			i+=2;
+		}
+		if(isprint(s[i]) || (s[i] >=0xA0 && s[i] <= 0xDF)){
+			s[j]=s[i];
+			j++;
+		}
+		i++;
+	}
+	s[j]='\0';
+	return s;
+}
+
+char * utf_del_uni(String &in){
+	in.toCharArray(_utf_del_uni_buf,65);
+	return utf_del_uni(_utf_del_uni_buf);
+}
