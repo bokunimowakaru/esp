90a670fe1a21396ac6922aa6cf2abf25f50a9ad5
Wataru KUNINO <xbee@dream.jp>
2017-10-29 10:12:11 +0900

    utf_del_uni のisprintの半角カナに対する曖昧解釈を回避

--- 2_example/example23_raspi/utf_del_uni.ino
+++ 2_example/example23_raspi/utf_del_uni.ino
@@ -1,20 +1,22 @@
 /*********************************************************************
-本ソースリストおよびソフトウェアは、ライセンスフリーです。
-UTF8の制御コードを除去します
-                               Copyright (c) 2014-2016 Wataru KUNINO
+本ソースリストおよびソフトウェアは、ライセンスフリーです。(詳細は別記)
+利用、編集、再配布等が自由に行えますが、著作権表示の改変は禁止します。
+
+UTF8の制御コードを除去します(UTF8をASCIIカナ文字へ変換するときに使用)
+
+                               Copyright (c) 2014-2017 Wataru KUNINO
                                http://www.geocities.jp/bokunimowakaru/
 *********************************************************************/
 
 void utf_del_uni(char *s){
-	byte i=0;
-	byte j=0;
+	unsigned int i=0;
+	unsigned int j=0;
 	while(s[i]!='\0'){
 		if((byte)s[i]==0xEF){
 			if((byte)s[i+1]==0xBE) s[i+2] += 0x40;
 			i+=2;
 		}
-	//	if(isprint(s[i])){
-		if(s[j]!='\r'){
+		if(isprint(s[i]) || (s[i] >=0xA0 && s[i] <= 0xDF)){
 			s[j]=s[i];
 			j++;
 		}
