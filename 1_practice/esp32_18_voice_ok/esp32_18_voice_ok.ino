cfa4e3951b2d80e605d2b0cb93613e40f54904cb
bokunimowakaru <xbee@dream.jp>
2017-12-31 10:10:39 +0900

    FTP追加と微修正

--- 1_practice/esp32_18_voice_ok/esp32_18_voice_ok.ino
+++ 1_practice/esp32_18_voice_ok/esp32_18_voice_ok.ino
@@ -1,11 +1,19 @@
 /*******************************************************************************
-Practice esp32 18 voice 【簡易音声認識子機】
+Practice esp32 18 voice OK 【簡易音声認識子機】
 
                                            Copyright (c) 2017-2018 Wataru KUNINO
 *******************************************************************************/
 
+/*
+※FTPサーバが必要です。
+　ラズベリーパイへ、FTPサーバをセットアップするには下記を実行してください。
+　~/esp/tools/ftp_setup.sh
+
+※FTPでは、ユーザ名やパスワード、データーが平文で転送されます。
+　インターネット上で扱う場合は、セキュリティに対する配慮が必要です。
+*/
+
 #include <WiFi.h>                           // ESP32用WiFiライブラリ
-#include <WiFiUdp.h>                        // UDP通信を行うライブラリ
 #include "uspeech.h"                        // 音声認識ライブラリ uspeech
 #define PIN_LED 2                           // IO 2へLEDを接続
 #define PIN_AIN 33                          // IO 33へマイクロホンを接続
@@ -13,23 +21,23 @@ Practice esp32 18 voice 【簡易音声認識子機】
 #define PASS "password"                     // パスワード
 #define SENDTO "192.168.0.255"              // 送信先のIPアドレス
 #define PORT 1024                           // 受信ポート番号
-#define DEVICE "voice_1,"                   // デバイス名(5文字+"_"+番号+",")
+#define DEVICE "sound_1,"                   // デバイス名(5文字+"_"+番号+",")
+#define FILENAME "/sound.wav"               // 音声ファイル名(アップロード用)
+#define FTP_TO   "192.168.0.10"             // FTP 送信先のIPアドレス
+#define FTP_USER "pi"                       // FTP ユーザ名(Raspberry Pi)
+#define FTP_PASS "password"                 // FTP パスワード(Raspberry Pi)
+#define FTP_DIR  "~"                        // FTP ディレクトリ(Raspberry Pi)
 
 signal voice(PIN_AIN);                      // 音声入力の有効化
 char phoneme[17];                           // 音声データ用の変数
-int phoneme_i=0,t=0,LED=0;                  // 音声データ数,待ち時間,LED状態保持
+int phoneme_i=0,t=0;                        // 音声データ数,待ち時間
 
 void setup(){
     WiFi.mode(WIFI_OFF); btStop();          // 無線LANとBluetoothをOFFに設定する
     pinMode(PIN_AIN,INPUT);                 // アナログ入力端子の設定
     pinMode(PIN_LED, OUTPUT);               // LEDを接続したポートを出力に
     digitalWrite(PIN_LED,HIGH);             // LEDを点灯する(起動を知らせる)
-    voice.f_enabled = true;                 // f音の検出を行う
-    voice.fconstant = 400;                  // f音の閾値設定
-    voice.econstant = 1;                    // e音の閾値設定
-    voice.aconstant = 2;                    // a音の閾値設定
-    voice.vconstant = 3;                    // v音の閾値設定
-    voice.shconstant = 4;                   // sh音の閾値設定
+    voice.micPowerThreshold=25;             // マイク入力閾値を下げる(初期値=50)
     voice.calibrate();                      // マイク入力のDC値の設定
     memset(phoneme,0,17);                   // 音声データ用の変数の初期化
     Serial.begin(115200);                   // 動作確認のためのシリアル出力開始
@@ -41,39 +49,34 @@ void loop(){
     if(p!=' '){                             // 認識結果が有音だった場合
         Serial.print(p);                    // 認識した音をシリアル出力表示
         if(phoneme_i > 0 && p==phoneme[phoneme_i-1]) return;    // 同音時に戻る
-        t=0; digitalWrite(PIN_LED,!LED);    // 終了待ち時間のリセット
+        t=0; digitalWrite(PIN_LED,HIGH);    // 終了待ち時間のリセット
         phoneme[phoneme_i++] = p;           // 認識音を音声データ配列変数へ保存
         if(phoneme_i<16) return;            // 配列16以内ならloopの先頭へ戻る
     }else{                                  // 認識結果が無音だったとき
         if(phoneme_i && t==0) Serial.print(' ');    // 空白をシリアル出力表示
-        t++; digitalWrite(PIN_LED,LED);     // 終了待ち時間を1つカウント
-        if(t < 16 || phoneme_i == 0)return; // 待ち時間変数tが16未満の時に戻る
+        t++; digitalWrite(PIN_LED,LOW);     // 終了待ち時間を1つカウント
+        if(t < 64 || phoneme_i == 0)return; // 待ち時間変数tが64未満の時に戻る
     }
     
     /**** 単語認識部 ****/
     char vr[9]; memset(vr,0,9);             // 認識結果の保存用変数定期と初期化
-    int led=-1;                             // 認識結果(未/ON/OFF)の保存用変数
-    char *spo=strchr(phoneme,'o');          // 音声データ内に「o」が含まれる
-    char *sph=strchr(phoneme,'h');          // 音声データ内に「h」が含まれる
-    char *spf=strchr(phoneme,'f');          // 音声データ内に「f」が含まれる
-    char *spv=strchr(phoneme,'v');          // 音声データ内に「v」が含まれる
-    if(spo>0){                              // 「o」が含まれていたとき
-        if(spo<spf || spo<spv) led=0;       // 「f」「v」が後にあればledを0へ
-        else led=1;                         // なければONと判定し変数ledを1へ
+    int ok=-1;                              // 認識結果(未/ON/OFF)の保存用変数
+    char *spo=strchr(phoneme,'o');          // 音声データ内に「o」が含まれるか？
+    char *sph=strchr(phoneme,'h');          // 音声データ内に「o」が含まれるか？
+    if(spo<sph) spo=sph;                    // oとhのうち後ろにある方をspoへ代入
+    if(spo>0){                              // 「o」または「h」が含まれている
+        char *spe=strchr(spo,'e');          // 「o」以降に「e」が含まれるか？
+        if(spe>0) ok=1;                     // 「o」が含まれて「e」が後にある
     }
-    if(sph>0){                              // 「h」が含まれていたとき
-        if(sph<spf || sph<spv) led=0;       // 「f」「v」が後にあればledを0へ
-        else led=1;                         // なければONと判定し変数ledを1へ
-    }
-    if(led>=0) digitalWrite(PIN_LED,led);   // 判定結果でLEDを制御
-      else strcpy(vr,",ERR");               // 結果無し時は文字列変数へERRを代入
-    if(led==0) strcpy(vr,",OFF");           // led=0のとき文字列変数へOFFを代入
-      else if(led==1) strcpy(vr,",ON");     // led=1のとき文字列変数へONを代入
+    if(ok>0) strcpy(vr,",OK");              // ok=1のとき文字列変数へOKを代入
+      else strcpy(vr,",ERR");               // その他のとき文字列変数へERRを代入
     Serial.println(vr);                     // 文字列変数をシリアル出力表示
     t=0; phoneme_i=0; memset(phoneme,0,17); // 時間、音声データ用の変数の初期化
-    if(led==LED || led<0) return;           // LED状態に変化がないときに戻る
-    LED=led;                                // LED状態を保持する変数LEDの更新
-    
+    if(ok<1) return;                        // ok=1でないときに戻る
+    digitalWrite(PIN_LED,HIGH);
+    int size=micToWave_rec(PIN_AIN);        // 録音を行う
+    digitalWrite(PIN_LED,LOW);
+    if(size <= 0) return;                   // サイズが0のときに戻る
     /**** Wi-Fi送信部 ****/
     WiFi.mode(WIFI_STA);                    // 無線LANをSTAモードに設定
     WiFi.begin(SSID,PASS);                  // 無線LANアクセスポイントへ接続
@@ -81,12 +84,8 @@ void loop(){
         digitalWrite(PIN_LED,!digitalRead(PIN_LED));            // LEDの点滅
         delay(100);                         // 待ち時間
     } Serial.println(WiFi.localIP());       // 本機のIPアドレスをシリアル出力
-    WiFiUDP udp;                            // UDP通信用のインスタンスを定義
-    udp.beginPacket(SENDTO, PORT);          // UDP送信先を設定
-    udp.print(DEVICE);                      // デバイス名を送信
-    udp.print(LED);                         // ON=1、OFF=0を送信
-    udp.println(vr);                        // 認識結果の文字列vrを送信
-    udp.endPacket(); delay(10);             // UDP送信の終了(実際に送信する)
+    micToWave_FTP(FILENAME);                // FTPによる送信を実行する
     WiFi.disconnect(true);                  // WiFiアクセスポイントを切断する
     WiFi.mode(WIFI_OFF);                    // 無線LANをOFFに設定する
+    Serial.println("done");                 // 終了表示
 }
---
+++ 1_practice/esp32_18_voice_ok/micToWave.ino
@@ -0,0 +1,250 @@
+/*******************************************************************************
+下記のサンプルを修正
+                                           Copyright (c) 2017-2018 Wataru KUNINO
+********************************************************************************
+Example 65 アナログ入力ポートから録音した音声を送信する
+
+********************************************************************************
+　マイク入力ポート：GPIO 34 ADC1_CH6
+　　　　　　　　　　IoT Express     A2ピン(P3 3番ピン)
+　　　　　　　　　　WeMos D1 R32        Analog 3ピン
+　　　　　　　　　　ESP-WROOM-32        6番ピン
+
+　録音開始入力ポート：GPIO 0 を Lレベルへ移行する
+　　　　　　　　　　IoT Express     BOOTスイッチを押す
+
+　受信用ソフト：toolsフォルダ内のget_sound.sh(Raspberry Pi用)などを使用する
+*******************************************************************************/
+
+#include <WiFi.h>                           // ESP32用WiFiライブラリ
+#define TIMEOUT 20000                       // タイムアウト 20秒
+#define SOUND_LEN 3*8000                    // 音声長 8000=約1秒
+
+byte snd[SOUND_LEN];                        // 音声録音用変数
+int snd_size=0;                             // 音声データの大きさ(バイト)
+const byte Wave_Header0[2]={0,0};
+const byte Wave_Header1[8]={16,0,0,0,1,0,1,0};
+const byte Wave_Header2[4]={0x40,0x1F,0,0}; // 8000Hz -> 00 00 1F 40
+const byte Wave_Header3[4]={1,0,8,0};
+
+int snd_rec(byte *snd, int len, int port){
+    unsigned long time,time_trig=0;
+    int i=0, wait_us=125;                   // 8kHz時 サンプリング間隔 125us
+    
+    while(i<len){
+        time=micros();
+        if(time < time_trig) continue;      // 未達時
+        snd[i]=(byte)(analogRead(PIN_AIN)>>4);  // アナログ入力 12ビット→8ビット
+        time_trig = time + wait_us;
+        i++;
+        if(time_trig < wait_us) break;      // 時間カウンタのオーバフロー
+    }
+    return i;
+}
+
+int micToWave_rec(int pin_ain){ 
+//  pinMode(pin_ain,INPUT);                 // アナログ入力端子の設定
+    unsigned long TIME=millis();
+    Serial.print("Recording... ");
+    snd_size=snd_rec(snd,SOUND_LEN,pin_ain);// 音声入力
+    Serial.print(millis()-TIME);
+    Serial.println("msec. Done");
+    if(snd_size<82) return 0;
+    return snd_size+44;
+}
+
+/*******************************************************************************
+FTP送信用クライアント for ESP-WROOM-02 Yahoo! ジオシティーズ対応版
+
+                                           Copyright (c) 2016-2017 Wataru KUNINO
+
+参考文献
+  ■ 本ソースコードの作成に当たり、下記の情報を参考にしました(2016/12/14)
+  |
+  | FTP passive client for IDE v1.0.1 and w5100/w5200
+  | Posted October 2012 by SurferTim
+  | Modified 6 June 2015 by SurferTim
+  |
+  | http://playground.arduino.cc/Code/FTP
+  
+  ■ Yahoo! ジオシティーズ (Geo Cities) へのアップロード等に対応するために
+  |下記への書き込み情報を参考にしました。
+  | esp8266/Arduino Ussues
+  | Add FTP Client library  #1183
+  |
+  | https://github.com/esp8266/Arduino/issues/1183
+*******************************************************************************/
+
+#define FTP_WAIT 1
+#define BUFFER_SIZE 128
+
+byte micToWave_FTP(const char *filename){
+    char ftpBuf[BUFFER_SIZE];
+    WiFiClient client;
+    WiFiClient dclient;
+    int i;
+    /*
+    File file = SPIFFS.open(filename,"r");
+    if(!file){
+        Serial.println("SPIFFS open fail");
+        return 11;
+    }
+    Serial.println("SPIFFS opened");
+    */
+    if (client.connect(FTP_TO,21)) {
+        Serial.println("Command connected");
+    }
+    if(eRcv(client,ftpBuf)) return 21;
+
+    sprintf(ftpBuf,"USER %s\r\n",FTP_USER);
+    client.print(ftpBuf);
+    delay(FTP_WAIT);
+    Serial.print(ftpBuf);
+    if(eRcv(client,ftpBuf)) return 22;
+
+    sprintf(ftpBuf,"PASS %s\r\n",FTP_PASS);
+    client.print(ftpBuf);
+    delay(FTP_WAIT);
+    Serial.println("PASS");
+    if(eRcv(client,ftpBuf)) return 23;
+    
+    client.print("Type I\r\n");
+    delay(FTP_WAIT);
+    Serial.println("Type i");
+    if(eRcv(client,ftpBuf)) return 25;
+
+    client.print("PASV\r\n");
+    delay(FTP_WAIT);
+    Serial.println("PASV");
+    delay(100);
+    if(eRcv(client,ftpBuf)) return 26;
+
+    char *tStr = strtok(ftpBuf,"(,");
+    if(tStr == NULL) return 27;
+    int array_pasv[6];
+    for (i = 0; i < 6; i++) {
+        tStr = strtok(NULL,"(,");
+        array_pasv[i] = atoi(tStr);
+        if(tStr == NULL){
+            Serial.println("Bad PASV Answer");
+            return 28;
+        }
+    }
+    
+    unsigned int hiPort,loPort;
+    hiPort = array_pasv[4] << 8;
+    loPort = array_pasv[5] & 255;
+    Serial.print("Data port: ");
+    hiPort = hiPort | loPort;
+    Serial.println(hiPort);
+    if (dclient.connect(FTP_TO,hiPort)) {
+        Serial.println("Data connected");
+    }else{
+        Serial.println("Data connection failed");
+        client.stop();
+//      file.close();
+        return 31;
+    }
+    sprintf(ftpBuf,"STOR %s%s\r\n",FTP_DIR,filename);
+    client.print(ftpBuf);
+    delay(FTP_WAIT);
+    Serial.print(ftpBuf);
+    if(eRcv(client,ftpBuf)){
+        dclient.stop();
+//      file.close();
+        return 32;
+    }
+    Serial.println("Writing");
+    i=0;
+    if(dclient.connected()){
+		dclient.write("RIFF",4);                 // RIFF                     ->[4]
+        i = snd_size + 44 - 8;                  // ファイルサイズ-8
+        dclient.write(i & 0xFF);                 // サイズ・最下位バイト     ->[5]
+        dclient.write((i>>8) & 0xFF);            // サイズ・第2位バイト      ->[6]
+        dclient.write(Wave_Header0,2);           // サイズ・第3-4位バイト    ->[8]
+        dclient.write("WAVEfmt ",8);             // "Wave","fmt "            ->[16]
+        dclient.write(Wave_Header1,8);           // 16,0,0,0,1,0,1,0         ->[24]
+        dclient.write(Wave_Header2,4);           // 68,172,0,0               ->[28]
+        dclient.write(Wave_Header2,4);           // 68,172,0,0               ->[32]
+        dclient.write(Wave_Header3,4);           // 1,0,8,0                  ->[36]
+        dclient.write("data",4);                 //  "data"                  ->[40]
+        i = snd_size + 44 - 126;                // ファイルサイズ-126
+        dclient.write(i & 0xFF);                 // サイズ・最下位バイト     ->[41]
+        dclient.write((i>>8) & 0xFF);            // サイズ・最下位バイト     ->[42]
+        dclient.write("\0\0",2);                 // サイズ3-4
+        for(i=0;i<snd_size;i++) dclient.write(snd[i]);
+        dclient.flush();                         // ESP32用 ERR_CONNECTION_RESET対策
+    }
+    dclient.stop();
+    Serial.println("Data disconnected");
+    if(eRcv(client,ftpBuf)) return 33;
+    client.print("QUIT\r\n");
+    delay(FTP_WAIT);
+    Serial.println("QUIT");
+    if(eRcv(client,ftpBuf)) return 91;
+    client.stop();
+    Serial.println("Command disconnected");
+//  file.close();
+//  Serial.println("SPIFFS closed");
+    return 0;
+}
+
+byte eRcv(WiFiClient &client,char *ftpBuf){
+    byte thisByte,i=0,len=0;
+
+    while(!client.available()){
+        delay(FTP_WAIT);
+        if(!client.connected()){
+            Serial.println("FTP:eRcv:disC");
+            return 11;
+        }
+        i++;
+        if(i>1000){ // 200ms以上必要
+            Serial.println("FTP:eRcv:noRes");
+            return 12;
+        }
+    }
+    while(client.connected()){
+        if(!client.available()){
+            delay(FTP_WAIT);
+            if(!client.available()) break;
+        }
+        thisByte = client.read();
+        if(thisByte==(byte)'\r');
+        else if(thisByte==(byte)'\n'){
+            Serial.write('>');
+            Serial.println(ftpBuf);
+            if(ftpBuf[0] >= '4'){
+                client.print("QUIT\r\n");
+                delay(FTP_WAIT);
+                Serial.println("QUIT");
+                return 1;
+            }
+            if(len>3 && ftpBuf[3] == ' '){
+                return 0;
+            }
+            len = 0;
+        }else if(len < BUFFER_SIZE - 1 ){
+            ftpBuf[len] = thisByte;
+            len++;      
+            ftpBuf[len] = 0;
+        }
+    }
+    return 0;
+}
+
+void efail(WiFiClient &client){
+    byte thisByte = 0;
+    client.print("QUIT\r\n");
+    delay(FTP_WAIT);
+    while(!client.available()){
+        if(!client.connected()) return;
+        delay(1);
+    }
+    while(client.available()){  
+        thisByte = client.read();
+        Serial.write(thisByte);
+    }
+    client.stop();
+    Serial.println("Command disconnected");
+}
