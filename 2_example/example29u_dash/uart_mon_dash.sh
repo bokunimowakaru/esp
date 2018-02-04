f7880f5b4810394de8e611c99fb9f3662a2ed947
Wataru KUNINO <xbee@dream.jp>
2018-02-04 23:59:52 +0900

    tab体裁

--- 2_example/example29u_dash/uart_mon_dash.sh
+++ 2_example/example29u_dash/uart_mon_dash.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# シリアルUARTからMACアドレスを受信し、送信する
+# シリアルUARTからMACアドレスを受信し、UDPで送信する
 # Copyright (c) 2018 Wataru KUNINO
 
 #UART="/dev/ttyUSB0"
@@ -18,40 +18,40 @@ MAC_NUM=${#MAC_LIST[*]}
 
 UARTS=(`ls /dev/ttyUSB*`) &> /dev/null
 if [ ${#UARTS[*]} -lt 1 ]; then
-	echo "no UART devices"
-	exit
+    echo "no UART devices"
+    exit
 fi
 for ((i=0; i < ${#UARTS[*]}; i++)); do
     if [ "$UART" = "${UARTS[i]}" ]; then
-		break
-	fi
+        break
+    fi
 done
 if [ $i -eq ${#UARTS[*]} ]; then
-	i=$(( i - 1 ))
-	UART=${UARTS[i]}
+    i=$(( i - 1 ))
+    UART=${UARTS[i]}
 fi
 echo "UART["${i}"]="${UART}
 stty --file ${UART} 115200 igncr
 while true; do                                      # 永久に繰り返し
-	UIN=`timeout 1 cat /dev/ttyUSB1`
-	delimiter=`echo $UIN|cut -d" " -f1`
-	mac=`echo $UIN|cut -d" " -f2`
-	if [ "${delimiter}" = "'" ] && [ -n $mac ]; then
-		echo -n ${mac}" "
-		for (( i=0; i < $MAC_NUM; i++ )); do
-			mac_array=(${MAC_LIST[i]})
-			mac_address=(${mac_array[0]})
-			mac_name=(${mac_array[1]})
-			if [ "${mac_address}" = "${mac}" ]; then
-				echo $mac_name
-				mac_csv=`echo ${mac_address}|tr ":" ","`
-				n=$(( i + 1 ))
-				sudo echo ${DEVICE}${n}","${mac_address}","${mac_name} > /dev/udp/${SENDTO}/${PORT}
-				break
-			fi
-		done
-		if [ $i -eq $MAC_NUM ]; then
-			echo
-		fi
-	fi
+    UIN=`timeout 1 cat /dev/ttyUSB1`
+    delimiter=`echo $UIN|cut -d" " -f1`
+    mac=`echo $UIN|cut -d" " -f2`
+    if [ "${delimiter}" = "'" ] && [ -n $mac ]; then
+        echo -n ${mac}" "
+        for (( i=0; i < $MAC_NUM; i++ )); do
+            mac_array=(${MAC_LIST[i]})
+            mac_address=(${mac_array[0]})
+            mac_name=(${mac_array[1]})
+            if [ "${mac_address}" = "${mac}" ]; then
+                echo $mac_name
+                mac_csv=`echo ${mac_address}|tr ":" ","`
+                n=$(( i + 1 ))
+                sudo echo ${DEVICE}${n}","${mac_address}","${mac_name} > /dev/udp/${SENDTO}/${PORT}
+                break
+            fi
+        done
+        if [ $i -eq $MAC_NUM ]; then
+            echo
+        fi
+    fi
 done
